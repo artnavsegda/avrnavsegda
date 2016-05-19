e3dda2b6fbe1969c699bf87348a2acc80ae80338
Art Navsegda <artnavsegda@gmail.com>
2016-05-19 05:22:20 +0300

    digital

--- xhello.c
+++ xhello.c
@@ -1,15 +1,27 @@
 #define F_CPU 2000000UL
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+
+ISR(PORTA_INT0_vect)
+{
+	PORTA.OUTTGL = PIN0_bm;
+}
 
 int main(void)
 {
-	PORTA.DIR = 0xFF;
+	unsigned char x = 0;
+	PORTA.DIR = PIN0_bm;
+	PORTA.PIN1CTRL = PORT_OPC_PULLUP_gc;
+	PORTA.INT0MASK = PIN1_bm;
+	PORTA.INTCTRL = PORT_INT0LVL_LO_gc;
+	PORTD.DIR = PIN4_bm;
+	PMIC.CTRL |= PMIC_LOLVLEN_bm;
+	sei();
 	while (1)
 	{
-		PORTA.OUT = 0xFF;
-		_delay_ms(1000);
-		PORTA.OUT = 0x00;
+		x = PORTA.IN;
+		PORTD.OUTTGL = PIN4_bm;
 		_delay_ms(1000);
 	}
 	return 0;

f598882335027724a7f1163f1ccf11cc30f07d8e
Art Navsegda <artnavsegda@gmail.com>
2016-05-20 22:32:10 +0300

    update

--- xmega/spi/spi/src/main.c
+++ xmega/spi/spi/src/main.c
@@ -30,6 +30,7 @@
  */
 #include <asf.h>
 
+#define DRDY_PIN IOPORT_CREATE_PIN(PORTC, 1)
 #define CS_PIN IOPORT_CREATE_PIN(PORTC, 4)
 #define MOSI_PIN IOPORT_CREATE_PIN(PORTC, 5)
 #define MISO_PIN IOPORT_CREATE_PIN(PORTC, 6)
@@ -39,6 +40,22 @@ struct spi_device SPI_ADC = {
 	//! Board specific select id
 	.id = CS_PIN
 };
+
+unsigned int result;
+
+ISR(PORTC_INT0_vect)
+{
+	spi_select_device(&SPIC, &SPI_ADC);
+	spi_put(&SPIC,0x08);
+	if (spi_get(&SPIC) == 8)
+	{
+		spi_put(&SPIC,0x38);
+		result = 0;
+		MSB(result) = spi_get(&SPIC);
+		LSB(result) = spi_get(&SPIC);
+	}
+	spi_deselect_device(&SPIC, &SPI_ADC);
+}
 
 int main (void)
 {
@@ -50,11 +67,18 @@ int main (void)
 
 	/* Insert application code here, after the board has been initialized. */
 
+	ioport_set_pin_dir(DRDY_PIN, IOPORT_DIR_INPUT);
 	ioport_set_pin_dir(CS_PIN, IOPORT_DIR_OUTPUT);
 	ioport_set_pin_dir(MOSI_PIN, IOPORT_DIR_OUTPUT);
 	ioport_set_pin_dir(MISO_PIN, IOPORT_DIR_INPUT);
 	ioport_set_pin_dir(SCK_PIN, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_mode(DRDY_PIN, IOPORT_MODE_PULLUP);
 	ioport_set_pin_mode(MISO_PIN, IOPORT_MODE_PULLUP);
+	ioport_set_pin_sense_mode(DRDY_PIN, IOPORT_SENSE_FALLING);
+	PORTC.INT0MASK = PIN1_bm;
+	PORTC.INTCTRL = PORT_INT0LVL_LO_gc;
+	PMIC.CTRL |= PMIC_LOLVLEN_bm;
+	cpu_irq_enable();
 	spi_master_init(&SPIC);
 	spi_master_setup_device(&SPIC, &SPI_ADC, SPI_MODE_3, 500000, 0);
 	spi_enable(&SPIC);

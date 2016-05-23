84de14b2a7ba5043ad8d03654a70fb439faa21e1
Art Navsegda <artnavsegda@gmail.com>
2016-05-23 09:12:56 +0300

    great success

--- xmega/spi/spi/src/main.c
+++ xmega/spi/spi/src/main.c
@@ -38,6 +38,7 @@
 void mediate(int income);
 void interrupt_init(void);
 void adc_init(void);
+uint8_t spi_gut(SPI_t *spi, uint8_t data);
 
 char string[10];
 
@@ -46,33 +47,39 @@ struct spi_device SPI_ADC = {
 	.id = SPIC_SS
 };
 
+#define AVERAGING 32
+#define STEP 5
+#define REFRESH 1000
 
-unsigned int massive[16];
+unsigned int massive[AVERAGING];
 int counter = 0;
 
 void mediate(int income)
 {
 	massive[counter] = income;
-	if (counter++ > 16)
+	if (counter++ > AVERAGING)
 		counter = 0; 
 }
 
 int num = 0;
 
+unsigned int result = 0;
+
 ISR(PORTC_INT0_vect)
 {
-	unsigned int result = 0;
 	LED_Toggle(LED2);
-	snprintf(string,10,"%d",num++);
+	//snprintf(string,10,"%d",num++);
 	//gfx_mono_draw_string(string,10,10,&sysfont);
 	spi_select_device(&SPIC, &SPI_ADC);
-	spi_put(&SPIC,0x08);
-	if (spi_get(&SPIC) == 8)
+	spi_gut(&SPIC,0x08);
+	if (spi_gut(&SPIC,CONFIG_SPI_MASTER_DUMMY) == 8)
 	{
-		spi_put(&SPIC,0x38);
-		MSB(result) = spi_get(&SPIC);
-		LSB(result) = spi_get(&SPIC);
+		spi_gut(&SPIC,0x38);
+		MSB(result) = spi_gut(&SPIC,0xFF);
+		LSB(result) = spi_gut(&SPIC,0xFF);
 		mediate(result);
+		//snprintf(string, sizeof(string), "%5ld ", (long)result-0x17CC);
+		//gfx_mono_draw_string(string,10,10,&sysfont);
 	}
 	spi_deselect_device(&SPIC, &SPI_ADC);
 }
@@ -102,12 +109,20 @@ void interrupt_init(void)
 
 void adc_init(void)
 {
+	uint8_t init[] = {0x20,0x0C,0x10,0x40};
 	spi_select_device(&SPIC, &SPI_ADC);
-	spi_put(&SPIC,0x20);
-	spi_put(&SPIC,0x0C);
-	spi_put(&SPIC,0x10);
-	spi_put(&SPIC,0x40);
+	spi_write_packet(&SPIC, "\x20\x0C\x10\x04", 4);
+	spi_write_packet(&SPIC, "\x60\x18\x3A\x00", 4);
+	spi_write_packet(&SPIC, "\x70\x89\x78\xD7", 4);
 	spi_deselect_device(&SPIC, &SPI_ADC);
+}
+
+uint8_t spi_gut(SPI_t *spi, uint8_t data)
+{
+	spi_put(spi,data);
+	while (!spi_is_rx_full(spi)) {
+	}
+	return spi_get(spi);
 }
 
 int main (void)
@@ -119,13 +134,13 @@ int main (void)
 	ioport_init();
 	board_init();
 	gfx_mono_init();
-	//interrupt_init();
+	interrupt_init();
 
-	ioport_configure_port_pin(&PORTC, PIN4_bm, IOPORT_INIT_HIGH | IOPORT_DIR_OUTPUT);
-	//ioport_configure_port_pin(&PORTC, PIN4_bm, IOPORT_PULL_UP | IOPORT_DIR_INPUT);
-	ioport_configure_port_pin(&PORTC, PIN5_bm, IOPORT_INIT_HIGH | IOPORT_DIR_OUTPUT);
-	ioport_configure_port_pin(&PORTC, PIN6_bm, IOPORT_DIR_INPUT);
-	ioport_configure_port_pin(&PORTC, PIN7_bm, IOPORT_INIT_HIGH | IOPORT_DIR_OUTPUT);
+	ioport_set_pin_dir(SPIC_SS, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_dir(SPIC_MOSI, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_dir(SPIC_MISO, IOPORT_DIR_INPUT);
+	ioport_set_pin_dir(SPIC_SCK, IOPORT_DIR_OUTPUT);
+	ioport_set_pin_mode(SPIC_MISO, IOPORT_MODE_PULLUP);
 
 	spi_master_init(&SPIC);
 	spi_master_setup_device(&SPIC, &SPI_ADC, SPI_MODE_3, 500000, 0);
@@ -135,25 +150,42 @@ int main (void)
 	/* Insert application code here, after the board has been initialized. */
 
 	ioport_set_value(LCD_BACKLIGHT_ENABLE_PIN, LCD_BACKLIGHT_ENABLE_LEVEL);
-	gfx_mono_draw_string("Hello world",10,10,&sysfont);
+	//gfx_mono_draw_string("Hello world",10,10,&sysfont);
 
-	unsigned int result = 0;
+	//unsigned int result = 0;
+	long x = 0;
+	int i;
 
-	while (1)
+	while (0)
 	{
 		spi_select_device(&SPIC, &SPI_ADC);
-		spi_put(&SPIC,0x08);
-		if (spi_get(&SPIC) == 8)
+		spi_gut(&SPIC,0x08);
+		if (spi_gut(&SPIC,CONFIG_SPI_MASTER_DUMMY) == 8)
 		{
-			spi_put(&SPIC,0x38);
-			spi_read_packet(&SPIC, &read_buffer, 2);
-			MSB(result) = read_buffer[0];
-			LSB(result) = read_buffer[1];
-			snprintf(string, 10, " %2.2X%2.2X", read_buffer[0],read_buffer[1]);
+			spi_gut(&SPIC,0x38);
+			//spi_read_packet(&SPIC, read_buffer, 2);
+			//MSB(result) = read_buffer[0];
+			//LSB(result) = read_buffer[1];
+			MSB(result) = spi_gut(&SPIC,0xFF);
+			LSB(result) = spi_gut(&SPIC,0xFF);
+			//snprintf(string, 10, " %2.2X%2.2X", read_buffer[0],read_buffer[1]);
+			//snprintf(string, 10, " %2.2X%2.2X", spi_gut(&SPIC,0xFF),spi_gut(&SPIC,0xFF));
+			snprintf(string, sizeof(string), "%5ld ", (long)result-0x17CC);
 			gfx_mono_draw_string(string,10,10,&sysfont);
 		}
-		ioport_toggle_pin(LED_STATUS_PIN);
+		//ioport_toggle_pin(LED_STATUS_PIN);
 		delay_ms(500);
+	}
+	while (1)
+	{
+		x = 0;
+		for(i=0;i<AVERAGING;i++)
+			x=x+massive[i];
+		snprintf(string, sizeof(string), "%5ld ", (long)result-0x17CC);
+		gfx_mono_draw_string(string,10,10,&sysfont);
+		snprintf(string, sizeof(string), "%5ld ", (x >> STEP)-0x17CC);
+		gfx_mono_draw_string(string,10,20,&sysfont);
+		delay_ms(REFRESH);
 	}
 
 }

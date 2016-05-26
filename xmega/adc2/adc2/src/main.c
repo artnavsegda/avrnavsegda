7a6fc0102172155ec10fc56bd6b5c76ec869bec8
Art Navsegda <artnavsegda@gmail.com>
2016-05-26 17:02:41 +0300

    adcsadasdkasd;kasf;lkasf;

--- xmega/adc2/adc2/src/main.c
+++ xmega/adc2/adc2/src/main.c
@@ -32,6 +32,11 @@
 
 void adc_init(void);
 
+static void adc_handler(ADC_t *adc, uint8_t ch_mask, adc_result_t result)
+{
+	return;
+}
+
 void adc_init(void)
 {
 	struct adc_config adc_conf;
@@ -41,6 +46,7 @@ void adc_init(void)
 	adc_set_conversion_parameters(&adc_conf, ADC_SIGN_OFF, ADC_RES_12, ADC_REF_BANDGAP);
 	adc_set_conversion_trigger(&adc_conf, ADC_TRIG_MANUAL, 1, 0);
 	adc_set_clock_rate(&adc_conf, 200000UL);
+	adc_set_callback(&ADCA, &adc_handler);
 	adcch_set_input(&adcch_conf, ADCCH_POS_PIN0, ADCCH_NEG_NONE, 1);
 	adc_write_configuration(&ADCA, &adc_conf);
 	adcch_write_configuration(&ADCA, ADC_CH0, &adcch_conf);
@@ -48,7 +54,6 @@ void adc_init(void)
 
 int main (void)
 {
-	uint16_t result;
 	/* Insert system clock initialization code here (sysclk_init()). */
 
 	board_init();
@@ -56,8 +61,4 @@ int main (void)
 	adc_init();
 
 	/* Insert application code here, after the board has been initialized. */
-	adc_enable(&ADCA);
-	adc_start_conversion(&ADCA, ADC_CH0);
-	adc_wait_for_interrupt_flag(&ADCA, ADC_CH0);
-	result = adc_get_result(&ADCA, ADC_CH0);
 }

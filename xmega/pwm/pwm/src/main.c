0ec50eff29a1e29a386fb55763e2dbb3ebc17695
Art Navsegda <artnavsegda@gmail.com>
2016-06-27 08:00:47 +0300

    pwm static

--- xmega/pwm/pwm/src/main.c
+++ xmega/pwm/pwm/src/main.c
@@ -32,9 +32,17 @@
 
 int main (void)
 {
+	struct pwm_config pwm_cfg;
+
 	/* Insert system clock initialization code here (sysclk_init()). */
 
+	sysclk_init();
 	board_init();
+	pwm_init(&pwm_cfg, PWM_TCE1, PWM_CH_A, 500);
+	pwm_start(&pwm_cfg, 10);
 
 	/* Insert application code here, after the board has been initialized. */
+	while(1) {
+		/* Nothing to do */
+	}
 }

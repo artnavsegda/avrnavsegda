d7ec34617eeed326dbf484c43e41140c56aaac84
Art Navsegda <artnavsegda@gmail.com>
2016-06-27 07:35:21 +0300

    fix&pwm

--- xmega/pwm/pwm/src/main.c
+++ xmega/pwm/pwm/src/main.c
@@ -30,38 +30,11 @@
  */
 #include <asf.h>
 
-ISR(PORTF_INT0_vect)
-{
-	ioport_toggle_pin(LCD_BACKLIGHT_ENABLE_PIN);
-}
-
 int main (void)
 {
 	/* Insert system clock initialization code here (sysclk_init()). */
 
-	sysclk_init();
-	ioport_init();
 	board_init();
 
 	/* Insert application code here, after the board has been initialized. */
-
-	ioport_set_pin_dir(LED2, IOPORT_DIR_OUTPUT);
-	ioport_set_pin_dir(LCD_BACKLIGHT_ENABLE_PIN, IOPORT_DIR_OUTPUT);
-	ioport_set_pin_dir(GPIO_PUSH_BUTTON_1,IOPORT_DIR_INPUT);
-	ioport_set_pin_mode(GPIO_PUSH_BUTTON_1, IOPORT_MODE_PULLUP);
-	ioport_set_pin_sense_mode(GPIO_PUSH_BUTTON_1, IOPORT_SENSE_FALLING);
-	PORTF.INT0MASK = PIN1_bm;
-	PORTF.INTCTRL = PORT_INT0LVL_LO_gc;
-	irq_initialize_vectors()
-	cpu_irq_enable();
-
-	//bool value;
-
-	while (1)
-	{
-		//value = ioport_get_pin_level(GPIO_PUSH_BUTTON_1);
-		//ioport_set_pin_level(LCD_BACKLIGHT_ENABLE_PIN,value);
-		ioport_toggle_pin(LED2);
-		delay_ms(500);
-	}
 }

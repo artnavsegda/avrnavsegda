3b0f873497621237727fffa74d937f2d93b8056b
Art Navsegda <artnavsegda@gmail.com>
2016-07-06 02:35:49 +0300

    difficult

--- xmega/i2c/i2c/src/main.c
+++ xmega/i2c/i2c/src/main.c
@@ -54,16 +54,66 @@ status_code_t i2c_send(TWI_t *twi, uint8_t addr, uint8_t memory, uint8_t content
 	return status;
 }
 
-uint8_t i2c_read(TWI_t *twi, uint8_t addr)
+status_code_t i2c_send_word(TWI_t *twi, uint8_t addr, uint8_t memory, uint16_t content) // ??????? i2c ??????
 {
-	uint8_t data_received[1];
-	twi_package_t packet_read = {
-		.chip         = 0x18,      // TWI slave bus address
-		.buffer       = data_received,        // transfer data destination buffer
-		.length       = 1                    // transfer data size (bytes)
-	};
-	if(twi_master_read(&TWIE, &packet_read) == TWI_SUCCESS)
-		return data_received[0];
+  status_code_t status;
+  uint8_t message[3];
+  twi_package_t packet = {
+    .chip         = addr,      // TWI slave bus address
+    .buffer       = message, // transfer data source buffer
+    .length       = 3  // transfer data size (bytes)
+  };
+  message[0] = memory;
+  message[1] = MSB(content);
+  message[2] = LSB(content);
+  status = twi_master_write(twi, &packet);
+  return status;
+}
+
+uint8_t i2c_read(TWI_t *twi, uint8_t addr, uint8_t memory)
+{
+   status_code_t status;
+   uint8_t message[1];
+   twi_package_t packet = {
+     .chip         = addr,      // TWI slave bus address
+     .buffer       = message,        // transfer data destination buffer
+     .length       = 1                    // transfer data size (bytes)
+   };
+   message[0] = memory;
+   status = twi_master_write(twi, &packet);
+   if(status == TWI_SUCCESS)
+   {
+     status_code_t status = twi_master_read(twi, &packet);
+     if(status == TWI_SUCCESS)
+		return message[0];
+   }
+   return status;
+}
+ 
+ uint16_t i2c_read_word(TWI_t *twi, uint8_t addr, uint8_t memory)
+ {
+   status_code_t status;
+   uint16_t recievedword;
+   uint8_t message[2];
+   twi_package_t packet = {
+     .chip         = addr,      // TWI slave bus address
+     .buffer       = message,        // transfer data destination buffer
+     .length       = 1                    // transfer data size (bytes)
+   };
+   message[0] = memory;
+   status = twi_master_write(twi, &packet);
+   if(status == TWI_SUCCESS)
+   {
+     packet.length = 2;
+     status_code_t status = twi_master_read(twi, &packet);
+     if(status == TWI_SUCCESS)
+     {
+		MSB(recievedword) = message[0];
+		LSB(recievedword) = message[1];
+		return recievedword;
+     }    
+	}
+	return status;
 }
 
 ISR(PORTF_INT0_vect)

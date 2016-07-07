2ab94583655d25bbf92fbe562768704e71ec837e
Art Navsegda <artnavsegda@gmail.com>
2016-07-07 07:36:53 +0300

    schiza

--- xmega/spi/spi/src/main.c
+++ xmega/spi/spi/src/main.c
@@ -715,26 +715,217 @@ int main (void)
 			i2c_send(&TWIE, 0x08, 100, false);
 			//zerotest();
 			zerodelay();
+			//entermode(ZERODELAY);
 		}
 		if (runcalibration)
 		{
 			i2c_send(&TWIE, 0x08, 99, false);
 			calibration();
+			//entermode(CALIBRATION)
 		}
 		if (runelemental)
 		{
 			i2c_send(&TWIE, 0x08, 101, false);
 			elemental();
+			//entermode(ELEMENTALMERCURY)
 		}
 		if (startpurge)
 		{
 			i2c_send(&TWIE, 0x08, 101, false);
 			purge();
+			//entermode(PURGE)
 		}
 		if (endpurge)
 		{
 			i2c_send(&TWIE, 0x08, 101, false);
 			total_mercury();
+			//entermode(TOTALMERCURY)
 		}
 	}
+}
+
+int modeseconds[30];
+
+void setupseconds(void)
+{
+	modeseconds[STARTLEVEL] = STARTLEVELSECONDS;
+	modeseconds[CELLDELAY] = CELLDELAYSECONDS;
+	modeseconds[CELLLEVEL] = CELLLEVELSECONDS;
+	modeseconds[ZERODELAY] = ZERODELAYSECONDS;
+	modeseconds[ZEROTEST] = ZEROTESTSECONDS;
+	modeseconds[PURGE] = PURGESECONDS;
+	modeseconds[TOTALMERCURYDELAY] = TOTALMERCURYDELAYSECONDS;
+	modeseconds[TOTALMERCURY] = TOTALMERCURYSECONDS;
+	modeseconds[ELEMENTALMERCURYDELAY] = ELEMENTALMERCURYDELAYSECONDS;
+	modeseconds[ELEMENTALMERCURY] = ELEMENTALMERCURYSECONDS;
+	modeseconds[PRECALIBRATIONDELAY] = PRECALIBRATIONDELAYSECONDS;
+	modeseconds[CALIBRATION] = CALIBRATIONSECONDS;
+	modeseconds[POSTCALIBRATIONDELAY] = POSTCALIBRATIONDELAYSECONDS;
+}
+
+void entermode(int modetoenter)
+{
+	modenumber = modetoenter;
+	i2c_send_word(&TWIE, 0x08, 8, modetoenter);
+	switch(modetoenter)
+	{
+		case STARTLEVEL:
+		break;
+		case CELLDELAY:
+		break;
+		case CELLLEVEL:
+		break;
+		case ZERODELAY:
+			i2c_send(&TWIE, 0x08, 4, true);
+			pca9557_set_pin_level(0x1a, VALVE_ZM, true);
+			ioport_set_pin_level(LED0,false);
+		return;
+		break;
+		case ZEROTEST:
+			i2c_send(&TWIE, 0x08, 4, true);
+			pca9557_set_pin_level(0x1a, VALVE_ZM, true);
+			ioport_set_pin_level(LED0,false);
+		break;
+		case PURGE:
+		break;
+		case TOTALMERCURYDELAY:
+		break;
+		case TOTALMERCURY:
+		break;
+		case ELEMENTALMERCURYDELAY:
+			pca9557_set_pin_level(U3, VALVE_TE, true);
+			ioport_set_pin_level(LED3,false);
+		break;
+		case ELEMENTALMERCURY:
+			pca9557_set_pin_level(U3, VALVE_TE, true);
+			ioport_set_pin_level(LED3,false);
+		break;
+		case PRECALIBRATIONDELAY:
+		break;
+		case CALIBRATION:
+			pca9557_set_pin_level(U3, VALVE_CM, true);
+			ioport_set_pin_level(LED1,false);
+		break;
+		case POSTCALIBRATIONDELAY:
+			pca9557_set_pin_level(U3, VALVE_CM, true);
+			ioport_set_pin_level(LED1,false);
+		break;
+		default:
+		break;
+	}
+	delay_s(modeseconds[modetoenter])
+	exitmode(modetoenter);
+}
+
+int averagevalue(int secondstoaverage)
+{
+	
+}
+
+int expectednominal;
+
+void exitmode(int modetoexit)
+{
+	switch(modetoexit)
+	{
+		case STARTLEVEL:
+		break;
+		case CELLDELAY:
+		break;
+		case CELLLEVEL:
+		break;
+		case ZERODELAY:
+		break;
+		case ZEROTEST:
+				ioport_set_pin_level(LED0,true);
+				pca9557_set_pin_level(U3, VALVE_ZM, false);
+				expectedzero = average(runner,DISPLAYUSE)>>STEP;
+				//expectedzero = averagevalue(modeseconds[modetoexit]);
+				i2c_send(&TWIE, 0x08, 4, false);
+		break;
+		case PURGE:
+		break;
+		case TOTALMERCURYDELAY:
+		break;
+		case TOTALMERCURY:
+		break;
+		case ELEMENTALMERCURYDELAY:
+		break;
+		case ELEMENTALMERCURY:
+				ioport_set_pin_level(LED2,true);
+				pca9557_set_pin_level(U3, VALVE_TE, false);
+				//expectedzero = average(runner,DISPLAYUSE)>>STEP;
+				//i2c_send(&TWIE, 0x08, 5, false);
+		break;
+		case PRECALIBRATIONDELAY:
+		break;
+		case CALIBRATION:
+				ioport_set_pin_level(LED1,true);
+				pca9557_set_pin_level(U3, VALVE_CM, false);
+				//expectedzero = average(runner,DISPLAYUSE)>>STEP;
+				expectednominal = average(runner,DISPLAYUSE)>>STEP;
+				//expectednominal = averagevalue(modeseconds[modetoexit]);
+				i2c_send(&TWIE, 0x08, 5, false);
+		break;
+		case POSTCALIBRATIONDELAY:
+				ioport_set_pin_level(LED1,true);
+				pca9557_set_pin_level(U3, VALVE_CM, false);
+				//expectedzero = average(runner,DISPLAYUSE)>>STEP;
+				i2c_send(&TWIE, 0x08, 5, false);
+		break;
+		default:
+		break;
+	}
+	entermode(sequence(modetoexit));
+}
+
+int sequence(int modetosequence)
+{
+	switch(modetosequence)
+	{
+		case STARTLEVEL:
+			return TOTALMERCURY;
+		break;
+		case CELLDELAY:
+			return CELLLEVEL;
+		break;
+		case CELLLEVEL:
+			return TOTALMERCURY;
+		break;
+		case ZERODELAY:
+			return ZEROTEST;
+		break;
+		case ZEROTEST:
+			return TOTALMERCURY;
+		break;
+		case PURGE:
+			return TOTALMERCURY;
+		break;
+		case TOTALMERCURYDELAY:
+			return TOTALMERCURY;
+		break;
+		case TOTALMERCURY:
+			return TOTALMERCURY;
+		break;
+		case ELEMENTALMERCURYDELAY:
+			return ELEMENTALMERCURY;
+		break;
+		case ELEMENTALMERCURY:
+			return TOTALMERCURY;
+		break;
+		case PRECALIBRATIONDELAY:
+			return CALIBRATION;
+		break;
+		case CALIBRATION:
+			//return POSTCALIBRATIONDELAY;
+			return TOTALMERCURY;
+		break;
+		case POSTCALIBRATIONDELAY:
+			return TOTALMERCURY;
+		break;
+		default:
+			return TOTALMERCURY;
+		break;
+	}
+	return TOTALMERCURY;
 }

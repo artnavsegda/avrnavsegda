00c2d7e51b05ab8099793daa6f6eb5d560137559
Art Navsegda <artnavsegda@gmail.com>
2016-07-13 22:29:32 +0300

    global async

--- xmega/interrupts/interrupts/src/main.c
+++ xmega/interrupts/interrupts/src/main.c
@@ -30,9 +30,19 @@
  */
 #include <asf.h>
 
-PROGMEM_DECLARE(char const, spinnertitle[]) = "Strings:";
-PROGMEM_DECLARE(char const, spinnertitle2[]) = "Numbers:";
-PROGMEM_DECLARE(char const, spinnertitle3[]) = "Numbers2:";
+PROGMEM_DECLARE(char const, spinnertitle[]) = "Start time:";
+PROGMEM_DECLARE(char const, spinnertitle2[]) = "Cell delay:";
+PROGMEM_DECLARE(char const, spinnertitle3[]) = "Cell time:";
+PROGMEM_DECLARE(char const, spinnertitle4[]) = "Zero delay:";
+PROGMEM_DECLARE(char const, spinnertitle5[]) = "Zero time:";
+PROGMEM_DECLARE(char const, spinnertitle6[]) = "Total delay:";
+PROGMEM_DECLARE(char const, spinnertitle7[]) = "Total time:";
+PROGMEM_DECLARE(char const, spinnertitle8[]) = "Ele delay:";
+PROGMEM_DECLARE(char const, spinnertitle9[]) = "Ele time:";
+PROGMEM_DECLARE(char const, spinnertitle10[]) = "Precal del:";
+PROGMEM_DECLARE(char const, spinnertitle11[]) = "Cal time:";
+PROGMEM_DECLARE(char const, spinnertitle12[]) = "Postcal del:";
+PROGMEM_DECLARE(char const, spinnertitle13[]) = "Purge tim:";
 PROGMEM_DECLARE(char const, spinner_choice1[]) = "String";
 PROGMEM_DECLARE(char const, spinner_choice2[]) = "String 2";
 PROGMEM_DECLARE(char const, spinner_choice3[]) = "String 3";
@@ -75,7 +85,20 @@ int16_t spinner_results[3];
 struct gfx_mono_spinctrl spinner1;
 struct gfx_mono_spinctrl spinner2;
 struct gfx_mono_spinctrl spinner3;
+struct gfx_mono_spinctrl spinner4;
+struct gfx_mono_spinctrl spinner5;
+struct gfx_mono_spinctrl spinner6;
+struct gfx_mono_spinctrl spinner7;
+struct gfx_mono_spinctrl spinner8;
+struct gfx_mono_spinctrl spinner9;
+struct gfx_mono_spinctrl spinner10;
+struct gfx_mono_spinctrl spinner11;
+struct gfx_mono_spinctrl spinner12;
+struct gfx_mono_spinctrl spinner13;
 struct gfx_mono_spinctrl_spincollection spinners;
+struct gfx_mono_spinctrl_spincollection spinners2;
+struct gfx_mono_spinctrl_spincollection spinners3;
+struct gfx_mono_spinctrl_spincollection spinners4;
 int16_t results[GFX_MONO_SPINCTRL_MAX_ELEMENTS_IN_SPINCOLLECTION];
 
 int mode = 0;
@@ -92,12 +115,60 @@ void process_input(uint8_t keycode)
 			mode = 1;
 			gfx_mono_spinctrl_spincollection_show(&spinners);
 			break;
+		case 1:
+			mode = 2;
+			gfx_mono_spinctrl_spincollection_show(&spinners2);
+			break;
+		case 2:
+			mode = 3;
+			gfx_mono_spinctrl_spincollection_show(&spinners3);
+			break;
+		case 3:
+			mode = 4;
+			gfx_mono_spinctrl_spincollection_show(&spinners4);
+			break;
 		default:
 			break;
 		}
 		break;
 	case 1:
 		spinner_status = gfx_mono_spinctrl_spincollection_process_key(&spinners, keycode, spinner_results);
+		switch (spinner_status)
+		{
+		case GFX_MONO_SPINCTRL_EVENT_FINISH:
+			mode = 0;
+			gfx_mono_menu_init(&mymenu);
+			break;
+		default:
+			break;
+		}
+		break;
+	case 2:
+		spinner_status = gfx_mono_spinctrl_spincollection_process_key(&spinners2, keycode, spinner_results);
+		switch (spinner_status)
+		{
+		case GFX_MONO_SPINCTRL_EVENT_FINISH:
+			mode = 0;
+			gfx_mono_menu_init(&mymenu);
+			break;
+		default:
+			break;
+		}
+		break;
+	case 3:
+		spinner_status = gfx_mono_spinctrl_spincollection_process_key(&spinners3, keycode, spinner_results);
+		switch (spinner_status)
+		{
+		case GFX_MONO_SPINCTRL_EVENT_FINISH:
+			mode = 0;
+			gfx_mono_menu_init(&mymenu);
+			break;
+		default:
+			break;
+		}
+		break;
+	case 4:
+		spinner_status = gfx_mono_spinctrl_spincollection_process_key(&spinners4, keycode, spinner_results);
 		switch (spinner_status)
 		{
 		case GFX_MONO_SPINCTRL_EVENT_FINISH:
@@ -160,14 +231,36 @@ int main (void)
 	gfx_mono_spinctrl_init(&spinner1, SPINTYPE_STRING, spinnertitle, spinner_choicestrings, 0, 3, 0);
 	gfx_mono_spinctrl_init(&spinner2, SPINTYPE_INTEGER,	spinnertitle2, NULL, -60, -41, 0);
 	gfx_mono_spinctrl_init(&spinner3, SPINTYPE_INTEGER,	spinnertitle3, NULL, 19999, 20200, 0);
+	gfx_mono_spinctrl_init(&spinner4, SPINTYPE_INTEGER,	spinnertitle4, NULL, -60, -41, 0);
+	gfx_mono_spinctrl_init(&spinner5, SPINTYPE_INTEGER,	spinnertitle5, NULL, -60, -41, 0);
+	gfx_mono_spinctrl_init(&spinner6, SPINTYPE_INTEGER,	spinnertitle6, NULL, -60, -41, 0);
+	gfx_mono_spinctrl_init(&spinner7, SPINTYPE_INTEGER,	spinnertitle7, NULL, -60, -41, 0);
+	gfx_mono_spinctrl_init(&spinner8, SPINTYPE_INTEGER,	spinnertitle8, NULL, -60, -41, 0);
+	gfx_mono_spinctrl_init(&spinner9, SPINTYPE_INTEGER,	spinnertitle9, NULL, -60, -41, 0);
+	gfx_mono_spinctrl_init(&spinner10, SPINTYPE_INTEGER, spinnertitle10, NULL, -60, -41, 0);
+	gfx_mono_spinctrl_init(&spinner11, SPINTYPE_INTEGER, spinnertitle11, NULL, -60, -41, 0);
+	gfx_mono_spinctrl_init(&spinner12, SPINTYPE_INTEGER, spinnertitle12, NULL, -60, -41, 0);
+	gfx_mono_spinctrl_init(&spinner13, SPINTYPE_INTEGER, spinnertitle13, NULL, -60, -41, 0);
 
 	// Initialize spincollection
 	gfx_mono_spinctrl_spincollection_init(&spinners);
+	gfx_mono_spinctrl_spincollection_init(&spinners2);
+	gfx_mono_spinctrl_spincollection_init(&spinners3);
+	gfx_mono_spinctrl_spincollection_init(&spinners4);
 
 	// Add spinners to spincollection
 	gfx_mono_spinctrl_spincollection_add_spinner(&spinner1, &spinners);
 	gfx_mono_spinctrl_spincollection_add_spinner(&spinner2, &spinners);
 	gfx_mono_spinctrl_spincollection_add_spinner(&spinner3, &spinners);
+	gfx_mono_spinctrl_spincollection_add_spinner(&spinner4, &spinners2);
+	gfx_mono_spinctrl_spincollection_add_spinner(&spinner5, &spinners2);
+	gfx_mono_spinctrl_spincollection_add_spinner(&spinner6, &spinners2);
+	gfx_mono_spinctrl_spincollection_add_spinner(&spinner7, &spinners3);
+	gfx_mono_spinctrl_spincollection_add_spinner(&spinner8, &spinners3);
+	gfx_mono_spinctrl_spincollection_add_spinner(&spinner9, &spinners3);
+	gfx_mono_spinctrl_spincollection_add_spinner(&spinner10, &spinners4);
+	gfx_mono_spinctrl_spincollection_add_spinner(&spinner11, &spinners4);
+	gfx_mono_spinctrl_spincollection_add_spinner(&spinner12, &spinners4);
 
 	/* Insert application code here, after the board has been initialized. */
 	while (true) {

86b2b84d3453f6d2235ad325d232800597c07e12
Art Navsegda <artnavsegda@gmail.com>
2016-09-04 22:58:53 +0300

    i2c new api

--- xmega/i2c/i2c/src/main.c
+++ xmega/i2c/i2c/src/main.c
@@ -30,89 +30,13 @@
  */
 #include <asf.h>
 #include "stdio.h"
+#include "i2c_api.h"
 
 char string[20];
 
 twi_master_options_t opt = {
 	.speed = 50000,
 };
-
-status_code_t i2c_send(TWI_t *twi, uint8_t addr, uint8_t memory, uint8_t content) // ??????? i2c ??????
-{
-	status_code_t status;
-	uint8_t message[2];
-	twi_package_t packet = {
-		.chip         = addr,      // TWI slave bus address
-		.buffer       = message, // transfer data source buffer
-		.length       = 2  // transfer data size (bytes)
-	};
-	message[0] = memory;
-	message[1] = content;
-	status = twi_master_write(twi, &packet);
-	return status;
-}
-
-status_code_t i2c_send_word(TWI_t *twi, uint8_t addr, uint8_t memory, uint16_t content) // ??????? i2c ??????
-{
-  status_code_t status;
-  uint8_t message[3];
-  twi_package_t packet = {
-    .chip         = addr,      // TWI slave bus address
-    .buffer       = message, // transfer data source buffer
-    .length       = 3  // transfer data size (bytes)
-  };
-  message[0] = memory;
-  message[1] = MSB(content);
-  message[2] = LSB(content);
-  status = twi_master_write(twi, &packet);
-  return status;
-}
-
-uint8_t i2c_read(TWI_t *twi, uint8_t addr, uint8_t memory)
-{
-   status_code_t status;
-   uint8_t message[1];
-   twi_package_t packet = {
-     .chip         = addr,      // TWI slave bus address
-     .buffer       = message,        // transfer data destination buffer
-     .length       = 1                    // transfer data size (bytes)
-   };
-   message[0] = memory;
-   status = twi_master_write(twi, &packet);
-   if(status == TWI_SUCCESS)
-   {
-     status_code_t status = twi_master_read(twi, &packet);
-     if(status == TWI_SUCCESS)
-		return message[0];
-   }
-   return status;
-}
- 
-uint16_t i2c_read_word(TWI_t *twi, uint8_t addr, uint8_t memory)
-{
-   status_code_t status;
-   uint16_t recievedword;
-   uint8_t message[2];
-   twi_package_t packet = {
-     .chip         = addr,      // TWI slave bus address
-     .buffer       = message,        // transfer data destination buffer
-     .length       = 1                    // transfer data size (bytes)
-   };
-   message[0] = memory;
-   status = twi_master_write(twi, &packet);
-   if(status == TWI_SUCCESS)
-   {
-     packet.length = 2;
-     status_code_t status = twi_master_read(twi, &packet);
-     if(status == TWI_SUCCESS)
-     {
-		MSB(recievedword) = message[0];
-		LSB(recievedword) = message[1];
-		return recievedword;
-     }    
-	}
-	return status;
-}
 
 ISR(PORTF_INT0_vect)
 {
@@ -126,6 +50,7 @@ ISR(PORTF_INT1_vect)
 
 int main (void)
 {
+	uint16_t ipaddress;
 	/* Insert system clock initialization code here (sysclk_init()). */
 
 	board_init();
@@ -152,7 +77,8 @@ int main (void)
 	//i2c_send_word(&TWIE, 0x08, 0x03, 0x1234); // register 03, contents 3f
 
 	while (true) {
-		snprintf(string,sizeof(string),"%3x",i2c_read(&TWIE, 0x8, 100));
+		ipaddress = i2c_read_double(&TWIE, 0x8, 100);
+		snprintf(string,sizeof(string),"IP: %d.%d.%d.%d",MSB0(ipaddress),MSB1(ipaddress),MSB2(ipaddress),MSB3(ipaddress));
 		gfx_mono_draw_string(string,10,10,&sysfont);
 		delay_ms(500);
 	}

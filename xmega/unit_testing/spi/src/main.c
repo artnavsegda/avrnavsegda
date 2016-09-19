661667e30150ad7a87a3d5f18bd2a8d60636d9a4
Art Navsegda <artnavsegda@gmail.com>
2016-09-19 16:37:44 +0300

    type code

--- xmega/unit_testing/spi/src/main.c
+++ xmega/unit_testing/spi/src/main.c
@@ -14,6 +14,18 @@ int main (void)
 
 	/* Insert application code here, after the board has been initialized. */
 	spi_select_device(&SPIC, &SPI_ADC);
-	spi_transfer(&SPIC, CONFIG_SPI_MASTER_DUMMY);
+
+	spi_write_packet(&SPIC, (uint8_t[]){0xFF,0xFF,0xFF,0xFF,0xFF}, 5);
+	spi_write_packet(&SPIC, (uint8_t[]){0x20,0x0C,0x10,0x04}, 4);
+	spi_write_packet(&SPIC, (uint8_t[]){0x60,0x18,0x3A,0x00}, 4);
+	spi_write_packet(&SPIC, (uint8_t[]){0x70,0x89,0x78,0xD7}, 4);
+
+	spi_transfer(&SPIC, 0x08);
+	if (spi_transfer(&SPIC,CONFIG_SPI_MASTER_DUMMY) == 8)
+	{
+		spi_transfer(&SPIC,0x38);
+		spi_read_packet(&SPIC, array, 2);
+	}
+
 	spi_deselect_device(&SPIC, &SPI_ADC);
 }

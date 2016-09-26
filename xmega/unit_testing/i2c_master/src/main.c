f59c8b26ea077387521d581d967ebfda3ad33f9f
Art Navsegda <artnavsegda@gmail.com>
2016-09-26 22:38:21 +0300

    this idunno

--- xmega/unit_testing/i2c_master/src/main.c
+++ xmega/unit_testing/i2c_master/src/main.c
@@ -1,28 +1,6 @@
 #include <asf.h>
 #include "i2c.h"
-
-void setup_init(void)
-{
-	sysclk_init();
-	board_init();
-	ioport_init();
-}
-
-void twi_configure(void)
-{
-	twi_master_options_t opt = { .speed = 50000 };
-	twi_master_setup(&TWIE, &opt);
-}
-
-void setup_configure(void)
-{
-	twi_configure();
-}
-
-void setup_enable(void)
-{
-	twi_master_enable(&TWIE);
-}
+#include "setup.h"
 
 int main (void)
 {
--- xmega/unit_testing/pca9557/src/main.c
+++ xmega/unit_testing/pca9557/src/main.c
@@ -1,32 +1,7 @@
 #include <asf.h>
 #include "i2c.h"
 #include "pca9557.h"
-
-void setup_init(void)
-{
-	sysclk_init();
-	board_init();
-	ioport_init();
-	pca9557_init(0x18);
-	pca9557_init(0x19);
-	pca9557_init(0x1a);
-}
-
-void twi_configure(void)
-{
-	twi_master_options_t opt = { .speed = 50000 };
-	twi_master_setup(&TWIE, &opt);
-}
-
-void setup_configure(void)
-{
-	twi_configure();
-}
-
-void setup_enable(void)
-{
-	twi_master_enable(&TWIE);
-}
+#include "setup.h"
 
 int main (void)
 {
@@ -47,5 +22,4 @@ int main (void)
 	//pca9557_set_pin_level(0x1a, 5, true);
 	//pca9557_set_pin_level(0x1a, 6, true);
 	//pca9557_set_pin_level(0x1a, 7, true);
-
 }

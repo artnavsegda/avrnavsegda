65d712f107f6d84bde5e92ee9927cf3a05d15008
Art Navsegda <artnavsegda@gmail.com>
2016-09-26 21:08:11 +0300

    roflin

--- xmega/unit_testing/rolling/src/main.c
+++ xmega/unit_testing/rolling/src/main.c
@@ -5,9 +5,28 @@
 extern int16_t adca_scan_results[8];
 extern int16_t adcb_scan_results[8];
 
+#define MASSIVE_SIZE 1000;
+
+struct massive {
+	uint16_t massive[MASSIVE_SIZE];
+	uint16_t position = 0;
+};
+
+uint16_t average(struct massive working, uint16_t amount)
+{
+	return 100;
+}
+
+void iterate(struct massive working, uint16_t value)
+{
+	working.position++;
+	working.massive[working.position] = value;
+}
+
 int main (void)
 {
 	char string[20];
+	struct massive adcone;
 
 	setup_init();
 	setup_configure();
@@ -15,14 +34,9 @@ int main (void)
 
 	/* Insert application code here, after the board has been initialized. */
 	while (true) {
-		snprintf(string,sizeof(string),"%3X %3X %3X %3X", adca_scan_results[0], adca_scan_results[1], adca_scan_results[2], adca_scan_results[3]);
+		iterate(adcone, adca_scan_results[0]);
+		snprintf(string,sizeof(string),"%d", average(adcone, 500));
 		gfx_mono_draw_string(string,8,0,&sysfont);
-		snprintf(string,sizeof(string),"%3X %3X %3X %3X", adca_scan_results[4], adca_scan_results[5], adca_scan_results[6], adca_scan_results[7]);
-		gfx_mono_draw_string(string,8,8,&sysfont);
-		snprintf(string,sizeof(string),"%3X %3X %3X %3X", adcb_scan_results[0], adcb_scan_results[1], adcb_scan_results[2], adcb_scan_results[3]);
-		gfx_mono_draw_string(string,8,16,&sysfont);
-		snprintf(string,sizeof(string),"%3X %3X %3X %3X", adcb_scan_results[4], adcb_scan_results[5], adcb_scan_results[6], adcb_scan_results[7]);
-		gfx_mono_draw_string(string,8,24,&sysfont);
-		//delay_ms(500);
+		delay_ms(1000);
 	}
 }

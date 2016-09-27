41e7b757dc269ba045d4798ef1e7191c4d60f02b
Art Navsegda <artnavsegda@gmail.com>
2016-09-27 05:42:21 +0300

    maga

--- xmega/unit_testing/settings/src/main.c
+++ xmega/unit_testing/settings/src/main.c
@@ -2,24 +2,7 @@
 #include <stdio.h>
 #include "i2c.h"
 #include "setup.h"
-
-#define I2C_IPADDRESS 100
-#define I2C_MACADDRESS 101
-
-struct mydatastruct
-{
-	uint8_t ip[4];
-	uint8_t mac[6];
-	uint16_t length_table[13];
-	uint8_t jump_table[13];
-	uint8_t ad7705_setup_register;
-	uint8_t ad7705_clock_register;
-	uint8_t ad7705_zero_scale[3];
-	uint8_t ad7705_full_scale[3];
-	float standard_concentration;
-	float c_twentie_five;
-	float kfactor;
-};
+#include "settings.h"
 
 int main (void)
 {

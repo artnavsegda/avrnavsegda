bebd676954ce91ba6660b1a1807ef438ab109c97
Art Navsegda <artnavsegda@gmail.com>
2016-09-27 06:19:39 +0300

    thisishit

--- xmega/unit_testing/sequencer/src/sequencer.c
+++ xmega/unit_testing/sequencer/src/sequencer.c
@@ -25,18 +25,97 @@ int modeseconds(int modeneed)
 	return 0;
 }
 
+int sequence(int modetosequence)
+{
+	switch(modetosequence)
+	{
+		case STARTLEVEL: return CELLDELAY;
+		case CELLDELAY: return CELLLEVEL;
+		case CELLLEVEL:	return TOTALMERCURYDELAY;
+		case ZERODELAY:	return ZEROTEST;
+		case ZEROTEST: return TOTALMERCURYDELAY;
+		case PURGE:	return TOTALMERCURYDELAY;
+		case TOTALMERCURYDELAY:	return TOTALMERCURY;
+		case TOTALMERCURY: return TOTALMERCURY;
+		case ELEMENTALMERCURYDELAY:	return ELEMENTALMERCURY;
+		case ELEMENTALMERCURY: return TOTALMERCURY;
+		case PRECALIBRATIONDELAY: return CALIBRATION;
+		case CALIBRATION: return POSTCALIBRATIONDELAY;
+		case POSTCALIBRATIONDELAY: return TOTALMERCURYDELAY;
+	}
+	return modetosequence;
+}
+
 void entermode(int modetoenter)
 {
 	currentmode = modetoenter;
 	timetoexitmode = modeseconds(modetoenter);
-}
-
-int sequence(int modetosequence)
-{
-	return 10;
+	switch(modetoenter)
+	{
+		case STARTLEVEL:
+		break;
+		case CELLDELAY:
+		break;
+		case CELLLEVEL:
+		break;
+		case ZERODELAY:
+		return;
+		break;
+		case ZEROTEST:
+		break;
+		case PURGE:
+		break;
+		case TOTALMERCURYDELAY:
+		break;
+		case TOTALMERCURY:
+		break;
+		case ELEMENTALMERCURYDELAY:
+		break;
+		case ELEMENTALMERCURY:
+		break;
+		case PRECALIBRATIONDELAY:
+		break;
+		case CALIBRATION:
+		break;
+		case POSTCALIBRATIONDELAY:
+		break;
+		default:
+		break;
+	}
 }
 
 void exitmode(int modetoexit)
 {
+	switch(modetoexit)
+	{
+		case STARTLEVEL:
+		break;
+		case CELLDELAY:
+		break;
+		case CELLLEVEL:
+		break;
+		case ZERODELAY:
+		break;
+		case ZEROTEST:
+		break;
+		case PURGE:
+		break;
+		case TOTALMERCURYDELAY:
+		break;
+		case TOTALMERCURY:
+		break;
+		case ELEMENTALMERCURYDELAY:
+		break;
+		case ELEMENTALMERCURY:
+		break;
+		case PRECALIBRATIONDELAY:
+		break;
+		case CALIBRATION:
+		break;
+		case POSTCALIBRATIONDELAY:
+		break;
+		default:
+		break;
+	}
 	entermode(sequence(modetoexit));
 }

b52223038de96a310fe5df5e5380909614c4b5ac
Art Navsegda <artnavsegda@gmail.com>
2016-09-29 02:12:17 +0300

    dis

--- xmega/GccBoardProject2/GccBoardProject2/src/ad7705.h
+++ xmega/GccBoardProject2/GccBoardProject2/src/ad7705.h
@@ -2,7 +2,13 @@
 #define AD7705_H_
 
 
-
+void ad7705_send_reset(SPI_t *spi, struct spi_device *device);
+void ad7705_set_clock_register(SPI_t *spi, struct spi_device *device, uint8_t contents);
+void ad7705_set_setup_register(SPI_t *spi, struct spi_device *device, uint8_t contents);
+void ad7705_set_scale_register(SPI_t *spi, struct spi_device *device, uint32_t contents);
+void ad7705_set_offset_register(SPI_t *spi, struct spi_device *device, uint32_t contents);
+uint8_t ad7705_get_communication_register(SPI_t *spi, struct spi_device *device);
+uint16_t ad7705_get_data_register(SPI_t *spi, struct spi_device *device);
 
 
 #endif /* AD7705_H_ */

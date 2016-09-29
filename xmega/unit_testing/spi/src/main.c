9d88a0719bce1ecaf92388125ef8ed1627d15325
Art Navsegda <artnavsegda@gmail.com>
2016-09-29 09:46:58 +0300

    this

--- xmega/unit_testing/spi/src/main.c
+++ xmega/unit_testing/spi/src/main.c
@@ -21,8 +21,8 @@ int main (void)
 
 	spi_write_packet(&SPIC, (uint8_t[]){0xFF,0xFF,0xFF,0xFF,0xFF}, 5);
 	spi_write_packet(&SPIC, (uint8_t[]){0x20,0x0C,0x10,0x04}, 4);
-	spi_write_packet(&SPIC, (uint8_t[]){0x60,0x18,0x3A,0x00}, 4);
-	spi_write_packet(&SPIC, (uint8_t[]){0x70,0x89,0x78,0xD7}, 4);
+	//spi_write_packet(&SPIC, (uint8_t[]){0x60,0x18,0x3A,0x00}, 4);
+	//spi_write_packet(&SPIC, (uint8_t[]){0x70,0x89,0x78,0xD7}, 4);
 
 	//spi_transfer(&SPIC, 0x08);
 	//if (spi_transfer(&SPIC,CONFIG_SPI_MASTER_DUMMY) == 8)

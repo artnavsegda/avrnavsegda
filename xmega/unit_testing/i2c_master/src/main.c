ffa950f2c778bc7364b882e4a26c6f7dd5e54ff3
Art Navsegda <artnavsegda@gmail.com>
2016-10-01 13:41:12 +0300

    i2c master

--- xmega/unit_testing/i2c_master/src/main.c
+++ xmega/unit_testing/i2c_master/src/main.c
@@ -2,18 +2,23 @@
 #include "i2c.h"
 #include "setup.h"
 
-//holding register addressing
-#define ELEMENTALMERCURYROW 128
-#define TOTALMERCURYROW 129
-#define OXIDIZEDMERCURYROW 130
-#define MONITORFLOW 131
-#define VACUUM 132
-#define DILUTIONPRESSURE 133
-#define BYPASSPRESSURE 134
-#define TEMPERATUREOFSPECTROMETER 135
-#define CODEOFACURRENTMODE 136
-#define ERRORSANDWARNINGS 137
-#define TOTALMERCURYCOEFFICENT 138
+struct MyObject {
+	char zero[4];
+	char one[6];
+	char two[26];
+	char three[13];
+};
+
+struct MyObject e;
+
+struct MyObject d = {
+	"4321",
+	"654321",
+	"abcdefghijklmnopqrstuwxyz",
+	"0123456789abc"
+};
+
+struct MyObject c;
 
 int main (void)
 {
@@ -24,6 +29,18 @@ int main (void)
 	setup_enable();
 
 	/* Insert application code here, after the board has been initialized. */
-	i2c_send_double(&TWIE, 0x08, TOTALMERCURYROW, 0.4554);
-	i2c_send_double(&TWIE, 0x08, MONITORFLOW, 0.656375);
+	i2c_read_array(&TWIE, 0x08, 0, 4, e.zero);
+	i2c_read_array(&TWIE, 0x08, 1, 6, e.one);
+	i2c_read_array(&TWIE, 0x08, 2, 26, e.two);
+	i2c_read_array(&TWIE, 0x08, 3, 13, e.three);
+
+	i2c_send_array(&TWIE, 0x08, 0, 4, d.zero);
+	i2c_send_array(&TWIE, 0x08, 1, 6, d.one);
+	i2c_send_array(&TWIE, 0x08, 2, 26, d.two);
+	i2c_send_array(&TWIE, 0x08, 3, 13, d.three);
+
+	i2c_read_array(&TWIE, 0x08, 0, 4, c.zero);
+	i2c_read_array(&TWIE, 0x08, 1, 6, c.one);
+	i2c_read_array(&TWIE, 0x08, 2, 26, c.two);
+	i2c_read_array(&TWIE, 0x08, 3, 13, c.three);
 }

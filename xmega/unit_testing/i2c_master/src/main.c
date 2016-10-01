4e4f381bd3780c1d13e41daa3ff5210d470ed434
Art Navsegda <artnavsegda@gmail.com>
2016-10-01 10:57:41 +0300

    i2c

--- xmega/unit_testing/i2c_master/src/main.c
+++ xmega/unit_testing/i2c_master/src/main.c
@@ -2,6 +2,19 @@
 #include "i2c.h"
 #include "setup.h"
 
+//holding register addressing
+#define ELEMENTALMERCURYROW 128
+#define TOTALMERCURYROW 129
+#define OXIDIZEDMERCURYROW 130
+#define MONITORFLOW 131
+#define VACUUM 132
+#define DILUTIONPRESSURE 133
+#define BYPASSPRESSURE 134
+#define TEMPERATUREOFSPECTROMETER 135
+#define CODEOFACURRENTMODE 136
+#define ERRORSANDWARNINGS 137
+#define TOTALMERCURYCOEFFICENT 138
+
 int main (void)
 {
 	/* Insert system clock initialization code here (sysclk_init()). */
@@ -11,6 +24,6 @@ int main (void)
 	setup_enable();
 
 	/* Insert application code here, after the board has been initialized. */
-	i2c_send(&TWIE, 0x1a, 0x03, 0x00);
-	i2c_send(&TWIE, 0x1a, 0x01, 0x00);
+	i2c_send_double(&TWIE, 0x08, TOTALMERCURYROW, 0.4554);
+	i2c_send_double(&TWIE, 0x08, MONITORFLOW, 0.656375);
 }

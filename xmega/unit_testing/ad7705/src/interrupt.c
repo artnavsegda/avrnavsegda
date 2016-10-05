e9a961b889be77e54e65bc862c93b6d920f54221
Art Navsegda <artnavsegda@gmail.com>
2016-10-05 23:55:48 +0300

    vlel

--- xmega/unit_testing/ad7705/src/interrupt.c
+++ xmega/unit_testing/ad7705/src/interrupt.c
@@ -1,9 +1,30 @@
 #include <asf.h>
+#include "interrupt.h"
 #include "spi_transfer.h"
 #include "ad7705.h"
 
 extern struct spi_device SPI_ADC;
+extern int16_t adc_scan_results[16];
 extern uint16_t adcdata;
+
+void adc_callback(ADC_t *adc, uint8_t ch_mask, adc_result_t result)
+{
+	static uint8_t current_adca_scan_channel = 0, current_adcb_scan_channel = 0;
+	if (adc == &ADCA) {
+		if (ch_mask & ADC_CH0) {
+			adc_scan_results[current_adca_scan_channel] = result;
+			current_adca_scan_channel++;
+			if (current_adca_scan_channel == 8)	current_adca_scan_channel = 0;
+		}
+	}
+	else if (adc == &ADCB) {
+		if (ch_mask & ADC_CH0) {
+			adc_scan_results[current_adcb_scan_channel+8] = result;
+			current_adcb_scan_channel++;
+			if (current_adcb_scan_channel == 8)	current_adcb_scan_channel = 0;
+		}
+	}
+}
 
 void ad7705_callback(void)
 {

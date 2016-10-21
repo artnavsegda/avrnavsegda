9cbf1aa52042f4a419eac9960d4f6d3e0eb4850e
Art Navsegda <artnavsegda@gmail.com>
2016-10-21 20:01:49 +0300

    i2c unit test

--- xmega/unit_testing/i2c_master/src/main.c
+++ xmega/unit_testing/i2c_master/src/main.c
@@ -3,27 +3,9 @@
 #include "i2c.h"
 #include "setup.h"
 
-struct MyObject {
-	char zero[4];
-	char one[6];
-	char two[26];
-	char three[13];
-};
-
-struct MyObject e;
-
-struct MyObject d = {
-	.zero = "432",
-	.one = "65432",
-	.two = "abcdefghijklmnopqrstuwxy",
-	.three = "0123456789ab"
-};
-
-struct MyObject c;
-
 int main (void)
 {
-	char string[20] = "shit";
+	char string[20];
 	/* Insert system clock initialization code here (sysclk_init()). */
 
 	setup_init();
@@ -46,35 +28,18 @@ int main (void)
 	//i2c_read_array(&TWIE, 0x08, 8, 5, string);
 	//gfx_mono_draw_string(string,8,8,&sysfont);
 
-	i2c_read_array(&TWIE, 0x08, 0, 4, e.zero);
-	i2c_read_array(&TWIE, 0x08, 1, 6, e.one);
-	i2c_read_array(&TWIE, 0x08, 2, 20, e.two);
-	i2c_read_array(&TWIE, 0x08, 3, 13, e.three);
 
-	gfx_mono_draw_string(e.zero,0,0,&sysfont);
-	gfx_mono_draw_string(e.one,0,8,&sysfont);
-	gfx_mono_draw_string(e.two,0,16,&sysfont);
-	gfx_mono_draw_string(e.three,0,24,&sysfont);
+	i2c_send(&TWIE, 0x1a, 0x03, 0x00);
+	i2c_send(&TWIE, 0x1a, 0x02, 0x00);
 
-	delay_ms(1000);
+	while (true)
+	{
+		i2c_send(&TWIE, 0x1a, 0x01, 0xFF);
+		LED_Toggle(LED2);
+		delay_ms(1000);
+		i2c_send(&TWIE, 0x1a, 0x01, 0x00);
+		LED_Toggle(LED2);
+		delay_ms(1000);
+	}
 
-	i2c_send_array(&TWIE, 0x08, 0, 4, d.zero);
-	i2c_send_array(&TWIE, 0x08, 1, 6, d.one);
-	i2c_send_array(&TWIE, 0x08, 2, 20, d.two);
-	i2c_send_array(&TWIE, 0x08, 3, 13, d.three);
-
-	i2c_read_array(&TWIE, 0x08, 0, 4, c.zero);
-	i2c_read_array(&TWIE, 0x08, 1, 6, c.one);
-	i2c_read_array(&TWIE, 0x08, 2, 20, c.two);
-	i2c_read_array(&TWIE, 0x08, 3, 13, c.three);
-
-	gfx_mono_draw_string(c.zero,0,0,&sysfont);
-	gfx_mono_draw_string(c.one,0,8,&sysfont);
-	gfx_mono_draw_string(c.two,0,16,&sysfont);
-	gfx_mono_draw_string(c.three,0,24,&sysfont);
-
-	i2c_send_array(&TWIE, 0x08, 0, 4, e.zero);
-	i2c_send_array(&TWIE, 0x08, 1, 6, e.one);
-	i2c_send_array(&TWIE, 0x08, 2, 20, e.two);
-	i2c_send_array(&TWIE, 0x08, 3, 13, e.three);
 }

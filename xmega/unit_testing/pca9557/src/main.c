b17c061ee8b20be83851292d0f4357803f945491
Art Navsegda <artnavsegda@gmail.com>
2016-10-22 03:29:18 +0300

    shit

--- xmega/unit_testing/pca9557/src/main.c
+++ xmega/unit_testing/pca9557/src/main.c
@@ -3,38 +3,41 @@
 #include "pca9557.h"
 #include "setup.h"
 
-struct pca9557_pin ignition = {
-	.address = 0x18,
-	.pin_number = 6,
-	.direction = PCA9557_DIR_OUTPUT
-};
+struct pca9557_pin x20_relay = { .address = 0x18, .pin_number = 0 };
+struct pca9557_pin x19_relay = { .address = 0x19, .pin_number = 0 };
+struct pca9557_pin x22 = { .address = 0x1a,	.pin_number = 0 };
+struct pca9557_pin vt4 = { .address = 0x1a, .pin_number = 5,};
+struct pca9557_pin elemental = { .address = 0x1a, .pin_number = 4};
+struct pca9557_pin calibration = { .address = 0x1a, .pin_number = 3};
+struct pca9557_pin zero = {	.address = 0x1a, .pin_number = 2};
+struct pca9557_pin ignition = {	.address = 0x1a, .pin_number = 1};
 
 struct abc1234 cell = {
 	.left_out.address = 0x18, .left_out.pin_number = 7,
 	.right_out.address = 0x18, .right_out.pin_number = 6,
 	.left_in.address = 0x18, left_in.pin_number = 5,
-	.right_in.address = 0x18, right_in.pin_number 4,
+	.right_in.address = 0x18, right_in.pin_number = 4,
 };
 
 struct abc1234 servo_2 = {
 	.left_out.address = 0x18, .left_out.pin_number = 3,
 	.right_out.address = 0x18, .right_out.pin_number = 2,
 	.left_in.address = 0x18, left_in.pin_number = 1,
-	.right_in.address = 0x19, right_in.pin_number 7,
+	.right_in.address = 0x19, right_in.pin_number = 7,
 };
 
 struct abc1234 servo_3 = {
 	.left_out.address = 0x19, .left_out.pin_number = 6,
 	.right_out.address = 0x19, .right_out.pin_number = 5,
 	.left_in.address = 0x19, left_in.pin_number = 4,
-	.right_in.address = 0x19, right_in.pin_number 3,
+	.right_in.address = 0x19, right_in.pin_number = 3,
 };
 
 struct abc1234 servo_4 = {
 	.left_out.address = 0x19, .left_out.pin_number = 2,
 	.right_out.address = 0x19, .right_out.pin_number = 1,
 	.left_in.address = 0x1a, left_in.pin_number = 7,
-	.right_in.address = 0x1a, right_in.pin_number 6,
+	.right_in.address = 0x1a, right_in.pin_number = 6,
 };
 
 abc1234_direction abc1234_get_direction(struct abc1234 servo)

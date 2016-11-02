fd884ffeee15c6a02f3c97051f8681686da88181
Art Navsegda <artnavsegda@gmail.com>
2016-11-02 22:15:35 +0300

    navsegda

--- xmega/unit_testing/ad7705+pca9557+tc/src/main.c
+++ xmega/unit_testing/ad7705+pca9557+tc/src/main.c
@@ -1,6 +1,10 @@
 #include <asf.h>
 #include <stdio.h>
 #include "setup.h"
+#include "pca9557.h"
+
+struct pca9557_pin x20_relay = { .address = 0x18, .pin_number = 0 };
+struct pca9557_pin x19_relay = { .address = 0x19, .pin_number = 0 };
 
 int main (void)
 {
@@ -9,8 +13,27 @@ int main (void)
 	setup_configure();
 	setup_enable();
 
+	pca9557_set_pin_dir(x19_relay.address, x19_relay.pin_number, PCA9557_DIR_OUTPUT);
+	pca9557_set_pin_dir(x20_relay.address, x20_relay.pin_number, PCA9557_DIR_OUTPUT);
+
 	/* Insert application code here, after the board has been initialized. */
 	do {
-		//nothings
+		
+		delay_s(5);
+		pca9557_set_pin_level(x19_relay.address, x19_relay.pin_number, false);
+		LED_On(LED0);
+
+		delay_s(5);
+		pca9557_set_pin_level(x19_relay.address, x19_relay.pin_number, true);
+		LED_Off(LED0);
+
+		delay_s(5);
+		pca9557_set_pin_level(x20_relay.address, x20_relay.pin_number, false);
+		LED_On(LED1);
+
+		delay_s(5);
+		pca9557_set_pin_level(x20_relay.address, x20_relay.pin_number, true);
+		LED_Off(LED1);
+
 	} while (1);
 }

c146b8c446573835e9da89d3c53140b3422cf343
Art Navsegda <artnavsegda@gmail.com>
2016-11-13 16:54:15 +0300

    static

--- xmega/unit_testing/pca9557+tc/src/interrupt.c
+++ xmega/unit_testing/pca9557+tc/src/interrupt.c
@@ -4,7 +4,10 @@
 
 void tc_callback(void)
 {
-	pca9557_toggle_pin_level(0x1a, 1);
+	static int i = 0;
+	pca9557_toggle_pin_level(0x1a, i++);
+	if (i == 8)
+		i = 0;
 	//LED_Toggle(LED2);
 	tc_clear_overflow(&TCC0);
 }

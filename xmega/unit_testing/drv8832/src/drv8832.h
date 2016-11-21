5c459d0f0fe1d8ac113fd97089c4a29377fa53dc
Art Navsegda <artnavsegda@gmail.com>
2016-11-21 06:30:49 +0300

    sheeeet

--- xmega/unit_testing/drv8832/src/drv8832.h
+++ xmega/unit_testing/drv8832/src/drv8832.h
@@ -1,8 +1,23 @@
+#include "pca9557.h"
+
 #ifndef DRV8832_H_
 #define DRV8832_H_
 
-enum drv8832_direction drv8832_get_direction(struct drv8832 servo)
-void drv8832_turn(struct drv8832 servo, enum drv8832_direction direction)
-void drv8832_init(struct drv8832 servo)
+enum drv8832_direction {
+	DRV8832_LEFT,
+	DRV8832_RIGHT,
+	DRV8832_UNKNOWN,
+};
+
+struct drv8832 {
+	struct pca9557_pin left_out;
+	struct pca9557_pin right_out;
+	struct pca9557_pin left_in;
+	struct pca9557_pin right_in;
+};
+
+enum drv8832_direction drv8832_get_direction(struct drv8832 servo);
+void drv8832_turn(struct drv8832 servo, enum drv8832_direction direction);
+void drv8832_init(struct drv8832 servo);
 
 #endif /* DRV8832_H_ */
--- xmega/unit_testing/drv8832/src/main.c
+++ xmega/unit_testing/drv8832/src/main.c
@@ -1,48 +1,10 @@
 #include <asf.h>
 #include "i2c.h"
-#include "pca9557.h"
+#include "drv8832.h"
 #include "setup.h"
 
-enum drv8832_direction {
-	DRV8832_LEFT,
-	DRV8832_RIGHT,
-	DRV8832_UNKNOWN,
-};
-
-struct drv8832 {
-	struct pca9557_pin left_out;
-	struct pca9557_pin right_out;
-	struct pca9557_pin left_in;
-	struct pca9557_pin right_in;
-};
-
-struct drv8832 cell = {
-	.left_out = { .address = 0x18, .pin_number = 7 },
-	.right_out = { .address = 0x18, .pin_number = 6 },
-	.left_in = { .address = 0x18, .pin_number = 5 },
-	.right_in = { .address = 0x18, .pin_number = 4 }
-};
-
-struct drv8832 servo_2 = {
-	.left_out = {.address = 0x18, .pin_number = 3},
-	.right_out = {.address = 0x18, .pin_number = 2},
-	.left_in = {.address = 0x18, .pin_number = 1},
-	.right_in = {.address = 0x19, .pin_number = 7}
-};
-
-struct drv8832 servo_3 = {
-	.left_out = {.address = 0x19, .pin_number = 6},
-	.right_out = {.address = 0x19, .pin_number = 5},
-	.left_in = {.address = 0x19, .pin_number = 4},
-	.right_in = {.address = 0x19, .pin_number = 3}
-};
-
-struct drv8832 servo_4 = {
-	.left_out = {.address = 0x19, .pin_number = 2},
-	.right_out = {.address = 0x19, .pin_number = 1},
-	.left_in = {.address = 0x1a, .pin_number = 7},
-	.right_in = {.address = 0x1a, .pin_number = 6}
-};
+extern struct drv8832 cell;
+extern struct drv8832 servo_2;
 
 int main (void)
 {

21fa78dc03f0902494cccb46147ad00fc4971a7a
Art Navsegda <artnavsegda@gmail.com>
2016-11-25 01:55:54 +0300

    process

--- xmega/GccBoardProject1/GccBoardProject1/src/main.c
+++ xmega/GccBoardProject1/GccBoardProject1/src/main.c
@@ -1,8 +1,35 @@
 #include <asf.h>
 #include "setup.h"
+#include <avr/io.h>
+
+void processcontrolbyte(uint8_t controlbyte)
+{
+	if ( _BV(1) )//ign
+	{
+
+	}
+	if ( _BV(2) )//pca
+	{
+
+	}
+	if ( _BV(3) )//drv
+	{
+
+	}
+	if ( _BV(4) )//pca
+	{
+
+	}
+	if ( _BV(5) )//pca
+	{
+
+	}
+}
 
 int main (void)
 {
+	uint8_t frame[5];
+	uint8_t controlbyte;
 	/* Insert system clock initialization code here (sysclk_init()). */
 
 	setup_init();
@@ -14,5 +41,14 @@ int main (void)
 	do {
 		/* Go to sleep, everything is handled by interrupts. */
 		//sleepmgr_enter_sleep();
+		if (usart_serial_read_packet(&USARTC0,(uint8_t *)&frame,5) == STATUS_OK)
+		{
+			if (frame[0] == 0xB5)
+			{
+				usart_serial_putchar(&USARTC0,0xB5);
+				controlbyte = frame[1];
+				processcontrolbyte(controlbyte);
+			}
+		}
 	} while (1);
 }

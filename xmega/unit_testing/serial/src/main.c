43042644b8a4fe1cd4087383a77f5fb0fd2b1474
Art Navsegda <artnavsegda@gmail.com>
2016-11-26 01:37:32 +0300

    oh god pain

--- xmega/unit_testing/serial/src/main.c
+++ xmega/unit_testing/serial/src/main.c
@@ -1,8 +1,19 @@
 #include <asf.h>
 #include "setup.h"
 
+ISR(USARTC0_RXC_vect)
+{
+	LED_Toggle(LED0);
+	usart_putchar(&USARTC0, usart_getchar(&USARTC0));
+	usart_clear_rx_complete(&USARTC0);
+	while (!usart_tx_is_complete(&USARTC0)) {
+	}
+	usart_clear_tx_complete(&USARTC0);
+}
+
 int main (void)
 {
+	//uint8_t frame[5];
 	/* Insert system clock initialization code here (sysclk_init()). */
 
 	setup_init();
@@ -11,4 +22,9 @@ int main (void)
 
 	/* Insert application code here, after the board has been initialized. */
 	printf("\n\rHello ATMEL World!\n\r");
+
+	do {
+		//if (usart_serial_read_packet(&USARTC0,(uint8_t *)&frame,5) == STATUS_OK)
+		//	LED_Toggle(LED0);
+	} while (1);
 }
--- xmega/unit_testing/serial/src/setup.c
+++ xmega/unit_testing/serial/src/setup.c
@@ -2,7 +2,7 @@
 #include "setup.h"
 
 const usart_serial_options_t usart_serial_options = {
-	.baudrate     = 57600,
+	.baudrate     = 9600,
 	.charlength   = USART_CHSIZE_8BIT_gc,
 	.paritytype   = USART_PMODE_DISABLED_gc,
 	.stopbits     = false
@@ -10,10 +10,10 @@ const usart_serial_options_t usart_serial_options = {
 
 void setup_init(void)
 {
-	sysclk_init();
 	board_init();
+	sysclk_init();
+	pmic_init();
 	ioport_init();
-	stdio_serial_init(&USARTC0, &usart_serial_options);
 	gfx_mono_init();
 }
 
@@ -22,8 +22,15 @@ void ioport_configure(void)
 	ioport_set_pin_level(LCD_BACKLIGHT_ENABLE_PIN, LCD_BACKLIGHT_ENABLE_LEVEL);
 }
 
+void usart_configure(void)
+{
+	stdio_serial_init(&USARTC0, &usart_serial_options);
+	usart_set_rx_interrupt_level(&USARTC0, USART_INT_LVL_LO);
+}
+
 void setup_configure(void)
 {
+	usart_configure();
 	ioport_configure();
 }
 

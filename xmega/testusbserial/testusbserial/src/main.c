8913a48d0d9c5e2fe706d51866da01500dffb064
Art Navsegda <artnavsegda@gmail.com>
2016-12-13 09:38:50 +0300

    testusbserial

--- xmega/testusbserial/testusbserial/src/main.c
+++ xmega/testusbserial/testusbserial/src/main.c
@@ -1,6 +1,19 @@
 #include <asf.h>
 #include "setup.h"
 
+int irqcount = 0;
+uint8_t buffer[100];
+
+void my_callback_rx_notify(uint8_t port)
+{
+	LED_Toggle(LED0);
+	irqcount++;
+	printf("IRQ:%d:length=%d ",irqcount,udi_cdc_get_nb_received_data());
+	while (udi_cdc_is_rx_ready())
+		printf(" 0x%X", udi_cdc_getc());
+	printf("\n\r");
+}
+
 int main (void)
 {
 	/* Insert system clock initialization code here (sysclk_init()). */
--- xmega/testusbserial/testusbserial/src/setup.c
+++ xmega/testusbserial/testusbserial/src/setup.c
@@ -1,6 +1,13 @@
 #include <asf.h>
 #include "setup.h"
 
+const usart_serial_options_t usart_serial_options = {
+	.baudrate     = 9600,
+	.charlength   = USART_CHSIZE_8BIT_gc,
+	.paritytype   = USART_PMODE_DISABLED_gc,
+	.stopbits     = false
+};
+
 void setup_init(void)
 {
 	sysclk_init();
@@ -15,9 +22,15 @@ void ioport_configure(void)
 	ioport_set_pin_level(LCD_BACKLIGHT_ENABLE_PIN, LCD_BACKLIGHT_ENABLE_LEVEL);
 }
 
+void usart_configure(void)
+{
+	stdio_serial_init(&USARTC0, &usart_serial_options);
+}
+
 void setup_configure(void)
 {
 	ioport_configure();
+	usart_configure();
 }
 
 void setup_enable(void)

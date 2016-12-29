ab6ab005cf1b04db3926cf834e1c07962097067f
Art Navsegda <artnavsegda@gmail.com>
2016-12-29 05:43:59 +0300

    yesche chevo

--- mikroc/serial/serial.c
+++ mikroc/serial/serial.c
@@ -5,9 +5,9 @@ char uart_rd;
   UARTC0_Init(9600);               // Initialize UART module at 9600 bps
   Delay_ms(100);                  // Wait for UART module to stabilize
 
-  UARTC0_Write_Text("Init");
-  UARTC0_Write(13);
-  UARTC0_Write(10);
+  UARTC0_Write_Text("Hello world from here!");
+//  UARTC0_Write(13);
+//  UARTC0_Write(10);
 
   while (1) {                     // Endless loop
    if (UARTC0_Data_Ready()) {      // If data is received,

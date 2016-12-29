a975c588363df00a37604f1e5ae4760e763fef56
Art Navsegda <artnavsegda@gmail.com>
2016-12-29 05:36:15 +0300

    blink

--- mikroc/blink/blink.c
+++ mikroc/blink/blink.c
@@ -1,3 +1,21 @@
 void main() {
-
+     while(1)
+     {
+      //led0
+      PORTR_DIR.B0 = 1;
+      PORTR_OUTTGL.B0 = 1;
+      delay_ms(500);
+      //led1
+      PORTR_DIR.B1 = 1;
+      PORTR_OUTTGL.B1 = 1;
+      delay_ms(500);
+      //led2
+      PORTD_DIR.B4 = 1;
+      PORTD_OUTTGL.B4 = 1;
+      delay_ms(500);
+      //led3
+      PORTD_DIR.B5 = 1;
+      PORTD_OUTTGL.B5 = 1;
+      delay_ms(500);
+     }
 }

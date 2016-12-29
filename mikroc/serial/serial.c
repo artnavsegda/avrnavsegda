4c59947b91c8e6007d7cedd22ec7bb5b6ac53469
Art Navsegda <artnavsegda@gmail.com>
2016-12-29 20:32:36 +0300

    kruto

--- mikroc/serial/serial.c
+++ mikroc/serial/serial.c
@@ -1,11 +1,16 @@
 void main() {
 
-char uart_rd;
+     char uart_rd;
 
-  UARTC0_Init(9600);               // Initialize UART module at 9600 bps
-  Delay_ms(100);                  // Wait for UART module to stabilize
+     OSC_CTRL = 0x02;                 // 32MHz internal RC oscillator
+     while(RC32MRDY_bit == 0);
+     CPU_CCP = 0xD8;
+     CLK_CTRL = 1;
 
-  UARTC0_Write_Text("Hello world from here!");
+     UARTC0_Init(9600);               // Initialize UART module at 9600 bps
+     Delay_ms(100);                  // Wait for UART module to stabilize
+
+     UARTC0_Write_Text("Hello world from here!");
 //  UARTC0_Write(13);
 //  UARTC0_Write(10);
 

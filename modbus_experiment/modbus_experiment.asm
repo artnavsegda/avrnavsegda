3f60aa084b2ef593a168f6786bf3ca5e10ba4c52
Art Navsegda <artnavsegda@gmail.com>
2016-12-31 05:17:46 +0300

    modbus experiment

---
+++ modbus_experiment/modbus_experiment.asm
@@ -0,0 +1,337 @@
+
+_PrintHandler:
+
+;modbus_experiment.c,29 :: 		void PrintHandler(char c)
+;modbus_experiment.c,31 :: 		UART_Write(c);
+	CALL       _UART_Write+0
+;modbus_experiment.c,32 :: 		}
+L_end_PrintHandler:
+	RET
+; end of _PrintHandler
+
+_SPI_Ethernet_UserTCP:
+	PUSH       R28
+	PUSH       R29
+	IN         R28, SPL+0
+	IN         R29, SPL+1
+	ADIW       R28, 6
+
+;modbus_experiment.c,37 :: 		unsigned int  SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
+	PUSH       R2
+	PUSH       R3
+	PUSH       R4
+	PUSH       R5
+	PUSH       R6
+	PUSH       R7
+; flags start address is: 16 (R16)
+	LDD        R16, Y+0
+	LDD        R17, Y+1
+; flags end address is: 16 (R16)
+;modbus_experiment.c,47 :: 		if(localPort != 502)             // I listen only to web request on port 80
+	LDI        R27, 1
+	CP         R7, R27
+	BRNE       L__SPI_Ethernet_UserTCP10
+	LDI        R27, 246
+	CP         R6, R27
+L__SPI_Ethernet_UserTCP10:
+	BRNE       L__SPI_Ethernet_UserTCP11
+	JMP        L_SPI_Ethernet_UserTCP0
+L__SPI_Ethernet_UserTCP11:
+;modbus_experiment.c,49 :: 		return(0) ;
+	LDI        R16, 0
+	LDI        R17, 0
+	JMP        L_end_SPI_Ethernet_UserTCP
+;modbus_experiment.c,50 :: 		}
+L_SPI_Ethernet_UserTCP0:
+;modbus_experiment.c,52 :: 		SPI_Ethernet_getBytes(buf, ERDPT, reqLength);
+	PUSH       R9
+	PUSH       R8
+	MOVW       R6, R8
+	CLR        R4
+	CLR        R5
+	LDI        R27, #lo_addr(_buf+0)
+	MOV        R2, R27
+	LDI        R27, hi_addr(_buf+0)
+	MOV        R3, R27
+	CALL       _SPI_Ethernet_getBytes+0
+	POP        R8
+	POP        R9
+;modbus_experiment.c,54 :: 		for(i = 0;i < reqLength; i++)
+; i start address is: 18 (R18)
+	LDI        R18, 0
+	LDI        R19, 0
+; i end address is: 18 (R18)
+L_SPI_Ethernet_UserTCP1:
+; i start address is: 18 (R18)
+	CP         R18, R8
+	CPC        R19, R9
+	BRLO       L__SPI_Ethernet_UserTCP12
+	JMP        L_SPI_Ethernet_UserTCP2
+L__SPI_Ethernet_UserTCP12:
+;modbus_experiment.c,57 :: 		PrintOut(PrintHandler, "%#2X ");
+	PUSH       R19
+	PUSH       R18
+	PUSH       R9
+	PUSH       R8
+	PUSH       R7
+	PUSH       R6
+	PUSH       R5
+	PUSH       R4
+	PUSH       R3
+	PUSH       R2
+	LDI        R27, hi_addr(?lstr_1_modbus_experiment+0)
+	PUSH       R27
+	LDI        R27, #lo_addr(?lstr_1_modbus_experiment+0)
+	PUSH       R27
+	LDI        R27, hi_addr(_PrintHandler+0)
+	PUSH       R27
+	LDI        R27, #lo_addr(_PrintHandler+0)
+	PUSH       R27
+	CALL       _PrintOut+0
+	IN         R26, SPL+0
+	IN         R27, SPL+1
+	ADIW       R26, 4
+	OUT        SPL+0, R26
+	OUT        SPL+1, R27
+	POP        R2
+	POP        R3
+	POP        R4
+	POP        R5
+	POP        R6
+	POP        R7
+	POP        R8
+	POP        R9
+	POP        R18
+	POP        R19
+;modbus_experiment.c,54 :: 		for(i = 0;i < reqLength; i++)
+	MOVW       R16, R18
+	SUBI       R16, 255
+	SBCI       R17, 255
+	MOVW       R18, R16
+;modbus_experiment.c,58 :: 		}
+; i end address is: 18 (R18)
+	JMP        L_SPI_Ethernet_UserTCP1
+L_SPI_Ethernet_UserTCP2:
+;modbus_experiment.c,60 :: 		UART_Write_Text("\n\r");
+	PUSH       R9
+	PUSH       R8
+	PUSH       R7
+	PUSH       R6
+	PUSH       R5
+	PUSH       R4
+	PUSH       R3
+	PUSH       R2
+	LDI        R27, #lo_addr(?lstr2_modbus_experiment+0)
+	MOV        R2, R27
+	LDI        R27, hi_addr(?lstr2_modbus_experiment+0)
+	MOV        R3, R27
+	CALL       _UART_Write_Text+0
+	POP        R2
+	POP        R3
+	POP        R4
+	POP        R5
+	POP        R6
+	POP        R7
+	POP        R8
+	POP        R9
+;modbus_experiment.c,62 :: 		return(0);
+	LDI        R16, 0
+	LDI        R17, 0
+;modbus_experiment.c,63 :: 		}
+;modbus_experiment.c,62 :: 		return(0);
+;modbus_experiment.c,63 :: 		}
+L_end_SPI_Ethernet_UserTCP:
+	POP        R7
+	POP        R6
+	POP        R5
+	POP        R4
+	POP        R3
+	POP        R2
+	POP        R29
+	POP        R28
+	RET
+; end of _SPI_Ethernet_UserTCP
+
+_SPI_Ethernet_UserUDP:
+	PUSH       R28
+	PUSH       R29
+	IN         R28, SPL+0
+	IN         R29, SPL+1
+	ADIW       R28, 6
+
+;modbus_experiment.c,65 :: 		unsigned int  SPI_Ethernet_UserUDP(unsigned char *remoteHost, unsigned int remotePort, unsigned int destPort, unsigned int reqLength, TEthPktFlags *flags)
+; flags start address is: 16 (R16)
+	LDD        R16, Y+0
+	LDD        R17, Y+1
+; flags end address is: 16 (R16)
+;modbus_experiment.c,67 :: 		return(0);
+	LDI        R16, 0
+	LDI        R17, 0
+;modbus_experiment.c,68 :: 		}
+L_end_SPI_Ethernet_UserUDP:
+	POP        R29
+	POP        R28
+	RET
+; end of _SPI_Ethernet_UserUDP
+
+_main:
+	LDI        R27, 255
+	OUT        SPL+0, R27
+	LDI        R27, 0
+	OUT        SPL+1, R27
+
+;modbus_experiment.c,73 :: 		void    main()
+;modbus_experiment.c,75 :: 		OSC_CTRL = 0x02;                 // 32MHz internal RC oscillator
+	PUSH       R2
+	PUSH       R3
+	PUSH       R4
+	PUSH       R5
+	PUSH       R6
+	PUSH       R7
+	LDI        R27, 2
+	STS        OSC_CTRL+0, R27
+;modbus_experiment.c,76 :: 		while(RC32MRDY_bit == 0)
+L_main4:
+	LDS        R27, RC32MRDY_bit+0
+	SBRC       R27, BitPos(RC32MRDY_bit+0)
+	JMP        L_main5
+;modbus_experiment.c,77 :: 		;
+	JMP        L_main4
+L_main5:
+;modbus_experiment.c,78 :: 		CPU_CCP = 0xD8;
+	LDI        R27, 216
+	OUT        CPU_CCP+0, R27
+;modbus_experiment.c,79 :: 		CLK_CTRL = 1;
+	LDI        R27, 1
+	STS        CLK_CTRL+0, R27
+;modbus_experiment.c,88 :: 		PORTC_DIR.B7 = 1;
+	LDS        R27, PORTC_DIR+0
+	SBR        R27, 128
+	STS        PORTC_DIR+0, R27
+;modbus_experiment.c,89 :: 		PORTC_DIR.B5 = 1;
+	LDS        R27, PORTC_DIR+0
+	SBR        R27, 32
+	STS        PORTC_DIR+0, R27
+;modbus_experiment.c,90 :: 		PORTC_DIR.B6 = 0;
+	LDS        R27, PORTC_DIR+0
+	CBR        R27, 64
+	STS        PORTC_DIR+0, R27
+;modbus_experiment.c,91 :: 		PORTB_DIR.B6 = 0;
+	LDS        R27, PORTB_DIR+0
+	CBR        R27, 64
+	STS        PORTB_DIR+0, R27
+;modbus_experiment.c,92 :: 		PORTB_OUT.B6 = 0;
+	LDS        R27, PORTB_OUT+0
+	CBR        R27, 64
+	STS        PORTB_OUT+0, R27
+;modbus_experiment.c,93 :: 		PORTC_DIR.B0 = 1;
+	LDS        R27, PORTC_DIR+0
+	SBR        R27, 1
+	STS        PORTC_DIR+0, R27
+;modbus_experiment.c,94 :: 		PORTC_DIR.B1 = 1;
+	LDS        R27, PORTC_DIR+0
+	SBR        R27, 2
+	STS        PORTC_DIR+0, R27
+;modbus_experiment.c,96 :: 		UARTC0_Init(115200);
+	LDS        R27, USARTC0_CTRLB+0
+	SBR        R27, 4
+	STS        USARTC0_CTRLB+0, R27
+	LDI        R27, 34
+	STS        USARTC0_BAUDCTRLA+0, R27
+	LDI        R27, 0
+	STS        USARTC0_BAUDCTRLB+0, R27
+	CALL       _UARTC0_Init+0
+;modbus_experiment.c,97 :: 		UART_Set_Active(&UARTC0_Read, &UARTC0_Write, &UARTC0_Data_Ready, &UARTC0_Tx_Idle);
+	LDI        R27, #lo_addr(_UARTC0_Tx_Idle+0)
+	MOV        R6, R27
+	LDI        R27, hi_addr(_UARTC0_Tx_Idle+0)
+	MOV        R7, R27
+	LDI        R27, #lo_addr(_UARTC0_Write+0)
+	MOV        R4, R27
+	LDI        R27, hi_addr(_UARTC0_Write+0)
+	MOV        R5, R27
+	LDI        R27, #lo_addr(_UARTC0_Read+0)
+	MOV        R2, R27
+	LDI        R27, hi_addr(_UARTC0_Read+0)
+	MOV        R3, R27
+	LDI        R27, hi_addr(_UARTC0_Data_Ready+0)
+	PUSH       R27
+	LDI        R27, #lo_addr(_UARTC0_Data_Ready+0)
+	PUSH       R27
+	CALL       _UART_Set_Active+0
+	IN         R26, SPL+0
+	IN         R27, SPL+1
+	ADIW       R26, 2
+	OUT        SPL+0, R26
+	OUT        SPL+1, R27
+;modbus_experiment.c,99 :: 		UART_Write_Text("Starting\n\r");
+	LDI        R27, #lo_addr(?lstr3_modbus_experiment+0)
+	MOV        R2, R27
+	LDI        R27, hi_addr(?lstr3_modbus_experiment+0)
+	MOV        R3, R27
+	CALL       _UART_Write_Text+0
+;modbus_experiment.c,101 :: 		SPIC_Init_Advanced(_SPI_MASTER, _SPI_FCY_DIV16, _SPI_CLK_LO_LEADING);
+	CLR        R4
+	LDI        R27, 1
+	MOV        R3, R27
+	LDI        R27, 16
+	MOV        R2, R27
+	CALL       _SPIC_Init_Advanced+0
+;modbus_experiment.c,102 :: 		SPI_Set_Active(&SPIC_Read,&SPIC_Write);
+	LDI        R27, #lo_addr(_SPIC_Write+0)
+	MOV        R4, R27
+	LDI        R27, hi_addr(_SPIC_Write+0)
+	MOV        R5, R27
+	LDI        R27, #lo_addr(_SPIC_Read+0)
+	MOV        R2, R27
+	LDI        R27, hi_addr(_SPIC_Read+0)
+	MOV        R3, R27
+	CALL       _SPI_Set_Active+0
+;modbus_experiment.c,103 :: 		Spi_Rd_Ptr = SPIC_Read;
+	LDI        R27, #lo_addr(_SPIC_Read+0)
+	STS        _SPI_Rd_Ptr+0, R27
+	LDI        R27, hi_addr(_SPIC_Read+0)
+	STS        _SPI_Rd_Ptr+1, R27
+;modbus_experiment.c,104 :: 		SPI_Ethernet_Init(myMacAddr, myIpAddr, Spi_Ethernet_FULLDUPLEX) ;
+	LDI        R27, 1
+	MOV        R6, R27
+	LDI        R27, #lo_addr(_myIpAddr+0)
+	MOV        R4, R27
+	LDI        R27, hi_addr(_myIpAddr+0)
+	MOV        R5, R27
+	LDI        R27, #lo_addr(_myMacAddr+0)
+	MOV        R2, R27
+	LDI        R27, hi_addr(_myMacAddr+0)
+	MOV        R3, R27
+	CALL       _SPI_Ethernet_Init+0
+;modbus_experiment.c,107 :: 		SPI_Ethernet_confNetwork(ipMask, gwIpAddr, dnsIpAddr) ;
+	LDI        R27, #lo_addr(_dnsIpAddr+0)
+	MOV        R6, R27
+	LDI        R27, hi_addr(_dnsIpAddr+0)
+	MOV        R7, R27
+	LDI        R27, #lo_addr(_gwIpAddr+0)
+	MOV        R4, R27
+	LDI        R27, hi_addr(_gwIpAddr+0)
+	MOV        R5, R27
+	LDI        R27, #lo_addr(_ipMask+0)
+	MOV        R2, R27
+	LDI        R27, hi_addr(_ipMask+0)
+	MOV        R3, R27
+	CALL       _SPI_Ethernet_confNetwork+0
+;modbus_experiment.c,109 :: 		while(1) // do forever
+L_main6:
+;modbus_experiment.c,114 :: 		SPI_Ethernet_doPacket() ;   // process incoming Ethernet packets
+	CALL       _SPI_Ethernet_doPacket+0
+;modbus_experiment.c,121 :: 		}
+	JMP        L_main6
+;modbus_experiment.c,122 :: 		}
+L_end_main:
+	POP        R7
+	POP        R6
+	POP        R5
+	POP        R4
+	POP        R3
+	POP        R2
+L__main_end_loop:
+	JMP        L__main_end_loop
+; end of _main
---
+++ modbus_experiment/modbus_experiment.cp
@@ -0,0 +1,126 @@
+#line 1 "C:/Users/artna/Documents/GitHub/avrnavsegda/modbus_experiment/modbus_experiment.c"
+#line 1 "c:/users/artna/documents/github/avrnavsegda/modbus_experiment/__ethenc28j60.h"
+#line 119 "c:/users/artna/documents/github/avrnavsegda/modbus_experiment/__ethenc28j60.h"
+typedef struct
+ {
+ unsigned char valid;
+ unsigned long tmr;
+ unsigned char ip[4];
+ unsigned char mac[6];
+ } SPI_Ethernet_arpCacheStruct;
+
+extern SPI_Ethernet_arpCacheStruct SPI_Ethernet_arpCache[];
+
+extern unsigned char SPI_Ethernet_macAddr[6];
+extern unsigned char SPI_Ethernet_ipAddr[4];
+extern unsigned char SPI_Ethernet_gwIpAddr[4];
+extern unsigned char SPI_Ethernet_ipMask[4];
+extern unsigned char SPI_Ethernet_dnsIpAddr[4];
+extern unsigned char SPI_Ethernet_rmtIpAddr[4];
+
+extern unsigned long SPI_Ethernet_userTimerSec;
+
+typedef struct {
+ unsigned canCloseTCP: 1;
+ unsigned isBroadcast: 1;
+} TEthPktFlags;
+#line 146 "c:/users/artna/documents/github/avrnavsegda/modbus_experiment/__ethenc28j60.h"
+extern void SPI_Ethernet_Init(unsigned char *resetPort, unsigned char resetBit, unsigned char *CSport, unsigned char CSbit, unsigned char *mac, unsigned char *ip, unsigned char fullDuplex);
+extern unsigned char SPI_Ethernet_doPacket();
+extern void SPI_Ethernet_putByte(unsigned char b);
+extern void SPI_Ethernet_putBytes(unsigned char *ptr, unsigned int n);
+extern void SPI_Ethernet_putConstBytes(const unsigned char *ptr, unsigned int n);
+extern unsigned char SPI_Ethernet_getByte();
+extern void SPI_Ethernet_getBytes(unsigned char *ptr, unsigned int addr, unsigned int n);
+extern unsigned int SPI_Ethernet_UserUDP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags * flags);
+extern unsigned int SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags * flags);
+extern void SPI_Ethernet_confNetwork(char *ipMask, char *gwIpAddr, char *dnsIpAddr);
+#line 7 "C:/Users/artna/Documents/GitHub/avrnavsegda/modbus_experiment/modbus_experiment.c"
+sfr sbit SPI_Ethernet_Rst at PORTA_OUT.B0;
+sfr sbit SPI_Ethernet_CS at PORTC_OUT.B0;
+sfr sbit SPI_Ethernet_Rst_Direction at PORTA_DIR.B0;
+sfr sbit SPI_Ethernet_CS_Direction at PORTC_DIR.B0;
+#line 16 "C:/Users/artna/Documents/GitHub/avrnavsegda/modbus_experiment/modbus_experiment.c"
+unsigned char myMacAddr[6] = {0x00, 0x14, 0xA5, 0x76, 0x19, 0x3f} ;
+unsigned char myIpAddr[4] = {192, 168, 1, 150} ;
+unsigned char gwIpAddr[4] = {192, 168, 1, 1 } ;
+unsigned char ipMask[4] = {255, 255, 255, 0 } ;
+unsigned char dnsIpAddr[4] = {192, 168, 1, 1 } ;
+#line 29 "C:/Users/artna/Documents/GitHub/avrnavsegda/modbus_experiment/modbus_experiment.c"
+void PrintHandler(char c)
+{
+ UART_Write(c);
+}
+
+
+unsigned char buf[100];
+
+unsigned int SPI_Ethernet_UserTCP(unsigned char *remoteHost, unsigned int remotePort, unsigned int localPort, unsigned int reqLength, TEthPktFlags *flags)
+{
+ int i;
+ unsigned int len;
+
+
+
+
+
+
+ if(localPort != 502)
+ {
+ return(0) ;
+ }
+
+ SPI_Ethernet_getBytes(buf,  0x00 , reqLength);
+
+ for(i = 0;i < reqLength; i++)
+ {
+
+ PrintOut(PrintHandler, "%#2X ");
+ }
+
+ UART_Write_Text("\n\r");
+
+ return(0);
+}
+
+unsigned int SPI_Ethernet_UserUDP(unsigned char *remoteHost, unsigned int remotePort, unsigned int destPort, unsigned int reqLength, TEthPktFlags *flags)
+{
+ return(0);
+}
+#line 73 "C:/Users/artna/Documents/GitHub/avrnavsegda/modbus_experiment/modbus_experiment.c"
+void main()
+{
+ OSC_CTRL = 0x02;
+ while(RC32MRDY_bit == 0)
+ ;
+ CPU_CCP = 0xD8;
+ CLK_CTRL = 1;
+#line 88 "C:/Users/artna/Documents/GitHub/avrnavsegda/modbus_experiment/modbus_experiment.c"
+ PORTC_DIR.B7 = 1;
+ PORTC_DIR.B5 = 1;
+ PORTC_DIR.B6 = 0;
+ PORTB_DIR.B6 = 0;
+ PORTB_OUT.B6 = 0;
+ PORTC_DIR.B0 = 1;
+ PORTC_DIR.B1 = 1;
+
+ UARTC0_Init(115200);
+ UART_Set_Active(&UARTC0_Read, &UARTC0_Write, &UARTC0_Data_Ready, &UARTC0_Tx_Idle);
+
+ UART_Write_Text("Starting\n\r");
+
+ SPIC_Init_Advanced(_SPI_MASTER, _SPI_FCY_DIV16, _SPI_CLK_LO_LEADING);
+ SPI_Set_Active(&SPIC_Read,&SPIC_Write);
+ Spi_Rd_Ptr = SPIC_Read;
+ SPI_Ethernet_Init(myMacAddr, myIpAddr,  0x01 ) ;
+
+
+ SPI_Ethernet_confNetwork(ipMask, gwIpAddr, dnsIpAddr) ;
+
+ while(1)
+ {
+#line 114 "C:/Users/artna/Documents/GitHub/avrnavsegda/modbus_experiment/modbus_experiment.c"
+ SPI_Ethernet_doPacket() ;
+#line 121 "C:/Users/artna/Documents/GitHub/avrnavsegda/modbus_experiment/modbus_experiment.c"
+ }
+}

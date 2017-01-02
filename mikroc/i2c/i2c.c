926ce509af728beb535d87cce83a9753a828cbfe
Art Navsegda <artnavsegda@gmail.com>
2017-01-02 19:02:12 +0300

    i2c

--- mikroc/i2c/i2c.c
+++ mikroc/i2c/i2c.c
@@ -1,39 +1,23 @@
+void TWIE_Send(unsigned char devaddr, unsigned char regaddr, unsigned char value)
+{
+        TWIE_Start();
+        TWIE_Write(devaddr << 1);
+        TWIE_Write(regaddr);
+        TWIE_Write(value);
+        TWIE_Stop();
+}
+
 void main()
 {
-     TWIE_Init(100000);
-     TWIE_Start();
-     TWIE_Write(0x1a << 1);          // send byte via TWI (device address + W)
-     TWIE_Write(0x03);             // send byte (address of location)
-     TWIE_Write(0x00);          // send data (data to be written)
-     TWIE_Stop();               // issue TWI stop signal
+	TWIE_Init(100000);
+	TWIE_Send(0x1a,0x03,0x00);
+	TWIE_Send(0x1a,0x02,0x00);
      
-     Delay_100ms();
-     
-     TWIE_Init(100000);
-     TWIE_Start();
-     TWIE_Write(0x1a << 1);          // send byte via TWI (device address + W)
-     TWIE_Write(0x02);             // send byte (address of location)
-     TWIE_Write(0x00);          // send data (data to be written)
-     TWIE_Stop();               // issue TWI stop signal
-     
-     while (1)
-     {
-       Delay_100ms();
-
-       TWIE_Init(100000);
-       TWIE_Start();
-       TWIE_Write(0x1a << 1);          // send byte via TWI (device address + W)
-       TWIE_Write(0x01);             // send byte (address of location)
-       TWIE_Write(0x00);          // send data (data to be written)
-       TWIE_Stop();               // issue TWI stop signal
-
-       Delay_100ms();
-
-       TWIE_Init(100000);
-       TWIE_Start();
-       TWIE_Write(0x1a << 1);          // send byte via TWI (device address + W)
-       TWIE_Write(0x01);             // send byte (address of location)
-       TWIE_Write(0xFF);          // send data (data to be written)
-       TWIE_Stop();               // issue TWI stop signal
-     }
+	while (1)
+	{
+		Delay_100ms();
+		TWIE_Send(0x1a,0x01,0x00);
+		Delay_100ms();
+		TWIE_Send(0x1a,0x01,0xFF);
+	}
 }

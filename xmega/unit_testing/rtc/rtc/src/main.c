0f946f4f0c8b990b387d4596296af442b4666d5c
Art Navsegda <artnavsegda@gmail.com>
2017-04-09 20:16:50 +0300

    rtc

--- xmega/unit_testing/rtc/rtc/src/main.c
+++ xmega/unit_testing/rtc/rtc/src/main.c
@@ -1,34 +1,11 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *
- */
-
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
 #include <asf.h>
+
+const usart_serial_options_t usart_serial_options = {
+	.baudrate     = 9600,
+	.charlength   = USART_CHSIZE_8BIT_gc,
+	.paritytype   = USART_PMODE_DISABLED_gc,
+	.stopbits     = false
+};
 
 int main (void)
 {
@@ -36,11 +13,23 @@ int main (void)
 
 	board_init();
 	sysclk_init();
-
-	rtc_vbat_system_check();
-
-	rtc_init(); 
+	stdio_serial_init(&USARTC0, &usart_serial_options);
 
 	/* Insert application code here, after the board has been initialized. */
-	rtc_get_time();
+	sysclk_enable_module(SYSCLK_PORT_GEN, SYSCLK_RTC);
+	while (RTC32.SYNCCTRL & RTC32_SYNCBUSY_bm);
+	
+	if (rtc_vbat_system_check(false) != VBAT_STATUS_OK)
+	{
+		rtc_init();
+	}
+	
+	printf("MCU started\n\r");
+	
+	while (true)
+	{
+		printf("RTC time is %lu\n\r",rtc_get_time());
+		delay_s(1);
+	}
+	
 }

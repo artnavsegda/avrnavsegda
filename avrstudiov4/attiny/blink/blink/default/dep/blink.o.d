ca6d91d22957b5373f222db3377aaef5e6af9abf
Art Navsegda <artnavsegda@gmail.com>
2017-06-08 22:00:47 +0300

    studio 4

---
+++ avrstudiov4/attiny/blink/blink/default/dep/blink.o.d
@@ -0,0 +1,43 @@
+blink.o: ../blink.c \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\io.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\inttypes.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\stdint.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\iotn2313a.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\common.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\version.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\lock.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\util\delay.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\math.h
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\io.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\inttypes.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\stdint.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\iotn2313a.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\common.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\version.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\util\delay.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\math.h:

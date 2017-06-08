db2bce8c95db55b1aae61335e38be3847b9f360c
Art Navsegda <artnavsegda@gmail.com>
2017-06-08 15:55:45 +0300

    timer

--- avrstudiov4/xmega/blink/blink/default/dep/blink.o.d
+++ avrstudiov4/xmega/blink/blink/default/dep/blink.o.d
@@ -1,43 +1,46 @@
 blink.o: ../blink.c \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iox256a3bu.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay_basic.h \
- c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/math.h
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\io.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\inttypes.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\stdint.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\iox256a3bu.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\common.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\version.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\xmega.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\lock.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\util\delay.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\math.h
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\io.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\sfr_defs.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\inttypes.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\stdint.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iox256a3bu.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\iox256a3bu.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\portpins.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\common.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\version.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\xmega.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\fuse.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\lock.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay_basic.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\util\delay.h:
 
-c:\program\ files\atmel\avr\ tools\avr\ toolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/math.h:
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\math.h:

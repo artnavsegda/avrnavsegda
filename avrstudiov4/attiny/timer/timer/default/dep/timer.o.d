e7e9ff2405d0cac295f32f4852cdcf62bf90e369
Art Navsegda <artnavsegda@gmail.com>
2017-06-09 15:28:00 +0300

    Timer v4

---
+++ avrstudiov4/attiny/timer/timer/default/dep/timer.o.d
@@ -0,0 +1,37 @@
+timer.o: ../timer.c \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\io.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\inttypes.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\stdint.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\iotn4313.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\common.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\version.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\lock.h \
+ c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\interrupt.h
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\io.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\inttypes.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\stdint.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\iotn4313.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\common.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\version.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\atmel\avr\ tools\avr\ toolchain\avr\include\avr\interrupt.h:

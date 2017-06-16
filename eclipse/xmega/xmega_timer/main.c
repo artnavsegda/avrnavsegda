b73a57378a868480a6fd1e5a000dcc5bcf84e8c7
Art Navsegda <artnavsegda@gmail.com>
2017-06-16 06:11:53 +0300

    fix timing

--- eclipse/xmega/xmega_timer/main.c
+++ eclipse/xmega/xmega_timer/main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	PORTD_DIRSET = _BV(4);
 	TCC0_PER = 0x2710;                // Set period 10000
-	TCC0_CTRLA = 0x05;                // Prescaler DIV64
+	TCC0_CTRLA = 0x06;                // Prescaler DIV64
 	TCC0_INTCTRLA = 2;                // Enable overflow interrupt
 	PMIC_CTRL = 2;                    // Enable medium level interrupts
 	sei();

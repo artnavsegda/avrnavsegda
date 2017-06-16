470980f0c48d5c29de88c41f587e3ba729a82700
Art Navsegda <artnavsegda@gmail.com>
2017-06-16 06:08:27 +0300

    xmega timer

--- eclipse/xmega/xmega_timer/main.c
+++ eclipse/xmega/xmega_timer/main.c
@@ -1,12 +1,18 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+
+ISR(TCC0_OVF_vect)
+{
+	PORTD_OUTTGL = _BV(4);
+}
 
 int main(void)
 {
 	PORTD_DIRSET = _BV(4);
-	while(1)
-	{
-		PORTD_OUTTGL = _BV(4);
-		_delay_ms(1000);
-	}
+	TCC0_PER = 0x2710;                // Set period 10000
+	TCC0_CTRLA = 0x05;                // Prescaler DIV64
+	TCC0_INTCTRLA = 2;                // Enable overflow interrupt
+	PMIC_CTRL = 2;                    // Enable medium level interrupts
+	sei();
+	while(1);
 }

0ddda935c543dfa0dae328434ca15abe367f35ec
Art Navsegda <artnavsegda@gmail.com>
2017-06-17 09:43:37 +0300

    adc

--- eclipse/atmega/adc/main.c
+++ eclipse/atmega/adc/main.c
@@ -1,19 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
-#define BAUD 9600
-#include <util/setbaud.h>
-
-static int uart_putchar(char c, FILE *stream);
-
-static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
-
-static int uart_putchar(char c, FILE *stream)
-{
-	loop_until_bit_is_set(UCSR0A, UDRE0);
-	UDR0 = c;
-	return 0;
-}
+#include "serial.h"
 
 int readadc(char chan)
 {
@@ -22,21 +10,7 @@ int readadc(char chan)
 
 int main(void)
 {
-    UBRR0H = UBRRH_VALUE;
-    UBRR0L = UBRRL_VALUE;
-
-#if USE_2X
-    UCSR0A |= _BV(U2X0);
-#else
-    UCSR0A &= ~(_BV(U2X0));
-#endif
-
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
-
-	stdout = &mystdout;
-
-	printf("Serial started\n\r");
+	startserial();
 
 	ADMUX = _BV(REFS0);
 	ADCSRA = _BV(ADEN) | 7;

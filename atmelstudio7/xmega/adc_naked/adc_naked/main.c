ae7b3a3d953b8a3c379cd7060c128f104784f427
Art Navsegda <artnavsegda@gmail.com>
2017-08-25 23:10:10 +0300

    adc

--- atmelstudio7/xmega/adc_naked/adc_naked/main.c
+++ atmelstudio7/xmega/adc_naked/adc_naked/main.c
@@ -1,18 +1,44 @@
-/*
- * adc_naked.c
- *
- * Created: 25-Aug-17 14:53:10
- * Author : artna
- */ 
-
 #include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdbool.h>
 
+static int uart_putchar(char c, FILE *stream);
+
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+static int uart_putchar(char c, FILE *stream)
+{
+	loop_until_bit_is_set(USARTC0.STATUS,USART_DREIF_bp);
+	USARTC0.DATA = c;
+	return 0;
+}
+
+void startserial(void)
+{
+	PORTC.DIRSET = PIN3_bm;
+	PORTC.DIRCLR = PIN2_bm;
+
+	USARTC0.CTRLC = (uint8_t) USART_CHSIZE_8BIT_gc | USART_PMODE_DISABLED_gc | false;
+	USARTC0.BAUDCTRLA = 12;
+
+	USARTC0.CTRLB |= USART_RXEN_bm;
+	USARTC0.CTRLB |= USART_TXEN_bm;
+
+	stdout = &mystdout;
+
+	printf("serial started\n\r");
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	unsigned int result;
+	startserial();
+
+	while(1)
+	{
+
+	}
+	return 0;
 }
 

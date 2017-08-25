a9c90f38e721368a4279503e6d375c23e090cb7e
Art Navsegda <artnavsegda@gmail.com>
2017-08-25 15:10:27 +0300

    tipatakkoroch

---
+++ atmelstudio6/xmega/adc_naked/adc_naked/adc_naked.c
@@ -0,0 +1,17 @@
+/*
+ * adc_naked.c
+ *
+ * Created: 25-Aug-17 15:08:19
+ *  Author: artna
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ atmelstudio7/xmega/adc_naked/adc_naked/main.c
@@ -0,0 +1,18 @@
+/*
+ * adc_naked.c
+ *
+ * Created: 25-Aug-17 14:53:10
+ * Author : artna
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

385702d4e1ad0c346f9018b9ab348b58ac8b43c2
Art Navsegda <artnavsegda@gmail.com>
2017-08-26 21:11:39 +0300

    adc

---
+++ atmelstudio7/atmega/adc/adc/main.c
@@ -0,0 +1,18 @@
+/*
+ * adc.c
+ *
+ * Created: 26-Aug-17 21:11:03
+ * Author : Art Navsegda
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

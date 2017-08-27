bb529dd46f2bed57db398d6d6a7be49e0d62cb02
Art Navsegda <artnavsegda@gmail.com>
2017-08-28 00:03:18 +0300

    blink asf

---
+++ eclipse/atmega/blink_asf/main.c
@@ -0,0 +1,17 @@
+#include <asf.h>
+
+int main (void)
+{
+	/* Insert system clock initialization code here (sysclk_init()). */
+
+	board_init();
+	sysclk_init();
+	ioport_configure_pin(ARDUINO13, IOPORT_DIR_OUTPUT);
+
+	/* Insert application code here, after the board has been initialized. */
+	while(1)
+	{
+		ioport_toggle_pin(ARDUINO13);
+		delay_s(1);
+	}
+}

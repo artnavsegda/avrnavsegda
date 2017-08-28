ed7ec3aa2c9afb2a80c2de9273e8b943f0adc48f
Art Navsegda <artnavsegda@gmail.com>
2017-08-28 13:53:23 +0300

    share project

---
+++ eclipse/atmega/adc-asf/main.c
@@ -0,0 +1,40 @@
+#include <asf.h>
+#include <stdio.h>
+
+static int uart_putchar(char c, FILE *stream);
+
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+const usart_rs232_options_t usart_serial_options = {
+	.baudrate     = 9600,
+	.charlength   = USART_CHSIZE_8BIT_gc,
+	.paritytype   = USART_PMODE_DISABLED_gc,
+	.stopbits     = false
+};
+
+static int uart_putchar(char c, FILE *stream)
+{
+	usart_putchar(&USART0, c);
+	return 0;
+}
+
+int main (void)
+{
+	/* Insert system clock initialization code here (sysclk_init()). */
+
+	sysclk_init();
+	board_init();
+	adc_init(ADC_PRESCALER_DIV128);
+	usart_init_rs232(&USART0, &usart_serial_options);
+	usart_set_baudrate(&USART0, 9600, 16000000);
+	stdout = &mystdout;
+
+	/* Insert application code here, after the board has been initialized. */
+	printf("Hello World!\n\r");
+	
+	while(1)
+	{
+		delay_s(1);
+		printf("adc value %d\n\r", adc_read_10bit(ADC_MUX_ADC0, ADC_VREF_AVCC));
+	}
+}

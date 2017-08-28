9e406a7b09c74bf0f84f399e80278b9c49967402
Art Navsegda <artnavsegda@gmail.com>
2017-08-28 04:50:07 +0300

    sechesh

--- atmelstudio7/atmega/adc_asf/adc_asf/src/main.c
+++ atmelstudio7/atmega/adc_asf/adc_asf/src/main.c
@@ -1,40 +1,40 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *
- */
-
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
 #include <asf.h>
+#include <stdio.h>
+
+static int uart_putchar(char c, FILE *stream);
+
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+const usart_rs232_options_t usart_serial_options = {
+	.baudrate     = 9600,
+	.charlength   = USART_CHSIZE_8BIT_gc,
+	.paritytype   = USART_PMODE_DISABLED_gc,
+	.stopbits     = false
+};
+
+static int uart_putchar(char c, FILE *stream)
+{
+	usart_putchar(&USART0, c);
+	return 0;
+}
 
 int main (void)
 {
 	/* Insert system clock initialization code here (sysclk_init()). */
 
+	sysclk_init();
 	board_init();
+	adc_init(ADC_PRESCALER_DIV128);
+	usart_init_rs232(&USART0, &usart_serial_options);
+	usart_set_baudrate(&USART0, 9600, 16000000);
+	stdout = &mystdout;
 
 	/* Insert application code here, after the board has been initialized. */
+	printf("Hello World!\n\r");
+	
+	while(1)
+	{
+		delay_s(1);
+		printf("adc value %d\n\r", adc_read_8bit(ADC_MUX_ADC0, ADC_VREF_AVCC));
+	}
 }

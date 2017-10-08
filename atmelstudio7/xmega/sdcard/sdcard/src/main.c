21fde731b8a0108ccaa26d87369922891088687e
Art Navsegda <artnavsegda@gmail.com>
2017-10-08 19:42:12 +0300

    death

--- atmelstudio7/xmega/sdcard/sdcard/src/main.c
+++ atmelstudio7/xmega/sdcard/sdcard/src/main.c
@@ -1,5 +1,6 @@
 #include <asf.h>
 #include <string.h>
+#include "sd_mmc_protocol.h"
 
 const usart_serial_options_t usart_serial_options = {
 	.baudrate     = 9600,
@@ -8,254 +9,54 @@ const usart_serial_options_t usart_serial_options = {
 	.stopbits     = false
 };
 
-/** Size of the file to write/read. */
-#define DATA_SIZE 2048
+//! \name Read/write access configurations
+//! @{
 
-/** Test settings: Number of bytes to test */
-#define TEST_SIZE   (4 * 1024)
+//! Offset before the end of memory card to start the test
+#define TEST_MEM_START_OFFSET  (1024lu * 1024lu * 4lu) // 4MB
 
-/** Example header */
-#define STRING_EOL    "\r"
-#ifndef BOARD_NAME
-# define BOARD_NAME "Undefined"
-#endif
-#define STRING_HEADER "-- FatFS Example --\r\n" \
-"-- "BOARD_NAME " --\r\n" \
-"-- Compiled: "__DATE__ " "__TIME__ " --"STRING_EOL
-#define MENU_HEADER "\n\r" \
-"---------------------------------------------------------\n\r"
-
-/* Read/write buffer */
-static uint8_t data_buffer[DATA_SIZE];
-
+//! Memory area size dedicated for the read/write test
+#define TEST_MEM_AREA_SIZE (1024lu * 1024lu * 1lu) // 1MB
 /**
- * \brief Scan files under a certain path.
- *
- * \param path Folder path.
- *
- * \return Scan result, 1: success.
+ * Size of each read or write access.
+ * Increasing this number can get higher R/W performance.
  */
-static FRESULT scan_files(char *path)
-{
-	FRESULT res;
-	FILINFO fno;
-	DIR dir;
-	int32_t i;
-	char *pc_fn;
-#if _USE_LFN
-	char c_lfn[_MAX_LFN + 1];
-	fno.lfname = c_lfn;
-	fno.lfsize = sizeof(c_lfn);
+#define TEST_MEM_ACCESS_SIZE  (8lu * SD_MMC_BLOCK_SIZE)
+
+#if TEST_MEM_START_OFFSET < TEST_MEM_AREA_SIZE
+#  error TEST_MEM_START_OFFSET must be higher than TEST_MEM_AREA_SIZE
+#endif
+#if TEST_MEM_ACCESS_SIZE > TEST_MEM_AREA_SIZE
+#  error TEST_MEM_AREA_SIZE must be higher than TEST_MEM_ACCESS_SIZE
 #endif
 
-	/* Open the directory */
-	res = f_opendir(&dir, path);
-	if (res == FR_OK) {
-		i = strlen(path);
-		for (;;) {
-			res = f_readdir(&dir, &fno);
-			if (res != FR_OK || fno.fname[0] == 0) {
-				break;
-			}
+//! The value used to generate test data
+#define TEST_FILL_VALUE_U32     (0x5500AAFFU)
 
-#if _USE_LFN
-			pc_fn = *fno.lfname ? fno.lfname : fno.fname;
-#else
-			pc_fn = fno.fname;
-#endif
-			if (*pc_fn == '.') {
-				continue;
-			}
+//! Buffer used by read/write tests
+COMPILER_WORD_ALIGNED
+static uint8_t buf_test[TEST_MEM_ACCESS_SIZE];
 
-			/* Check if it is a directory type */
-			if (fno.fattrib & AM_DIR) {
-				sprintf(&path[i], "/%s", pc_fn);
-				res = scan_files(path);
-				if (res != FR_OK) {
-					break;
-				}
+//! Read and write test length of CIA in bytes
+#define TEST_CIA_SIZE           (0x16)
 
-				path[i] = 0;
-			} else {
-				printf("%s/%s\n\r", path, pc_fn);
-			}
-		}
-	}
+//! Buffer for test SDIO data
+static uint8_t buf_cia[TEST_CIA_SIZE];
+//! @}
 
-	return res;
-}
-
-/**
- * \brief Do file system tests.
- *
- * \param disk_dev_num disk number
- *
- * \return Test result, 1: success.
- */
-static uint8_t run_fatfs_test(uint32_t disk_dev_num)
-{
-	uint32_t i;
-	UINT byte_to_read;
-	UINT byte_read;
-#if _FS_READONLY == 0
-	UINT byte_written;
-#endif
-
-	FRESULT res;
-	DIR dirs;
-	TCHAR root_directory[3] = "0:";
-	/* File name to be validated */
-	TCHAR file_name[12] = "0:Basic.bin";
-
-	/* Declare these as static to avoid stack usage.
-	 * They each contain an array of maximum sector size.
-	 */
-	static FATFS fs;
-	static FIL file_object;
-
-	root_directory[0] = '0' + disk_dev_num;
-	file_name[0] = '0' + disk_dev_num;
-
-	/* Mount disk*/
-	printf("-I- Mount disk %d\n\r", (int)disk_dev_num);
-	/* Clear file system object */
-	memset(&fs, 0, sizeof(FATFS));
-	res = f_mount(disk_dev_num, &fs);
-	if (res != FR_OK) {
-		printf("-E- f_mount pb: 0x%X\n\r", res);
-		return 0;
-	}
-
-	/* Test if the disk is formatted */
-	res = f_opendir(&dirs, root_directory);
-	if (res == FR_OK) {
-		/* Erase sd card to reformat it ? */
-		puts("-I- The disk is already formatted.\r");
-
-		/* Display the file tree */
-		puts("-I- Display files contained in the memory :\r");
-		strcpy((char *)data_buffer, root_directory);
-		scan_files((char *)data_buffer);
-
-#if _FS_TINY == 0
-		puts("-I- The disk will be re-formatted.\r");
-
-		res = FR_NO_FILESYSTEM;
-#endif
-	}
-
-	if (res == FR_NO_FILESYSTEM) {
-#if _FS_READONLY == 0
-		/* Format disk */
-		printf("-I- Format disk %d\n\r", (int)disk_dev_num);
-		puts("-I- Please wait a moment during formatting...\r");
-		res = f_mkfs(disk_dev_num, /* Drv */
-				0, /* FDISK partition */
-				512); /* AllocSize */
-		puts("-I- Disk format finished !\r");
-		if (res != FR_OK) {
-			printf("-E- f_mkfs pb: 0x%X\n\r", res);
-			return 0;
-		}
-
-#else
-		puts("-I- Please run Full version FAT FS test first\r");
-		return 0;
-#endif
-	} else if (FR_OK != res) {
-		printf("-E- f_opendir pb: 0x%X\n\r", res);
-		return 0;
-	}
-
-#if _FS_READONLY == 0
-	/* Create a new file */
-	printf("-I- Create a file : \"%s\"\n\r", file_name);
-	res = f_open(&file_object, (char const *)file_name,
-			FA_CREATE_ALWAYS | FA_WRITE);
-	if (res != FR_OK) {
-		printf("-E- f_open create pb: 0x%X\n\r", res);
-		return 0;
-	}
-
-	/* Write a checkerboard pattern in the buffer */
-	for (i = 0; i < sizeof(data_buffer); i++) {
-		if ((i & 1) == 0) {
-			data_buffer[i] = (i & 0x55);
-		} else {
-			data_buffer[i] = (i & 0xAA);
-		}
-	}
-	puts("-I- Write file\r");
-	for (i = 0; i < TEST_SIZE; i += DATA_SIZE) {
-		res = f_write(&file_object, data_buffer, DATA_SIZE,
-				&byte_written);
-
-		if (res != FR_OK) {
-			printf("-E- f_write pb: 0x%X\n\r", res);
-			return 0;
-		}
-	}
-
-	/* Close the file */
-	puts("-I- Close file\r");
-	res = f_close(&file_object);
-	if (res != FR_OK) {
-		printf("-E- f_close pb: 0x%X\n\r", res);
-		return 0;
-	}
-#endif
-
-	/* Open the file */
-	printf("-I- Open the same file : \"%s\"\n\r", file_name);
-	res = f_open(&file_object, (char const *)file_name,
-			FA_OPEN_EXISTING | FA_READ);
-	if (res != FR_OK) {
-		printf("-E- f_open read pb: 0x%X\n\r", res);
-		return 0;
-	}
-
-	/* Read file */
-	puts("-I- Read file\r");
-	memset(data_buffer, 0, DATA_SIZE);
-	byte_to_read = file_object.fsize;
-
-	for (i = 0; i < byte_to_read; i += DATA_SIZE) {
-		res = f_read(&file_object, data_buffer, DATA_SIZE, &byte_read);
-		if (res != FR_OK) {
-			printf("-E- f_read pb: 0x%X\n\r", res);
-			return 0;
-		}
-	}
-
-	/* Close the file*/
-	puts("-I- Close file\r");
-	res = f_close(&file_object);
-	if (res != FR_OK) {
-		printf("-E- f_close pb: 0x%X\n\r", res);
-		return 0;
-	}
-
-	/* Compare the read data with the expected data */
-	for (i = 0; i < sizeof(data_buffer); i++) {
-		if (!((((i & 1) == 0) && (data_buffer[i] == (i & 0x55))) ||
-				(data_buffer[i] == (i & 0xAA)))) {
-			printf(
-					"Invalid data at data[%u] (expected 0x%02X, read 0x%02X)\n\r",
-					(unsigned int)i,
-					(unsigned int)(((i & 1) == 0) ? (i & 0x55) : (i & 0xAA)),
-					(unsigned int)data_buffer[i]);
-		}
-	}
-	puts("-I- File data Ok !\r");
-
-	return 1;
-}
+static void main_display_info_card(uint8_t slot);
+static void main_test_memory(uint8_t slot);
+static void main_test_sdio(uint8_t slot);
+static void main_dump_buffer(uint8_t *data_buffer, uint32_t length);
 
 int main (void)
 {
-	uint32_t disk_dev_num;
+	uint8_t slot = 0;
+	sd_mmc_err_t err;
 	/* Insert system clock initialization code here (sysclk_init()). */
 
+	irq_initialize_vectors();
+	cpu_irq_enable();
 	sysclk_init();
 	board_init();
 	rtc_init();
@@ -265,15 +66,332 @@ int main (void)
 	printf("sd mmc start\n\r");
 
 	/* Insert application code here, after the board has been initialized. */
-	for (disk_dev_num = 0; disk_dev_num < get_nb_lun(); disk_dev_num++) {
-		if (run_fatfs_test(disk_dev_num)) {
-			printf("-I- DISK %d Test passed !\n\r\n\r", (int)disk_dev_num);
-			} else {
-			printf("-F- DISK %d Test Failed !\n\r\n\r", (int)disk_dev_num);
+	printf("\x0C\n\r-- SD/MMC/SDIO Card Example --\n\r");
+	printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
+	while (1) {
+		if (slot == sd_mmc_nb_slot()) {
+			slot = 0;
 		}
+		printf("Please plug an SD, MMC or SDIO card in slot %d.\n\r", slot+1);
+
+		// Wait for a card and ready
+		do {
+			err = sd_mmc_check(slot);
+			if ((SD_MMC_ERR_NO_CARD != err)
+			&& (SD_MMC_INIT_ONGOING != err)
+			&& (SD_MMC_OK != err)) {
+				printf("Card install FAILED\n\r");
+				printf("Please unplug and re-plug the card.\n\r");
+				while (SD_MMC_ERR_NO_CARD != sd_mmc_check(slot)) {
+				}
+			}
+		} while (SD_MMC_OK != err);
+		
+		// Display basic card information
+		main_display_info_card(slot);
+		
+		/* Test the card */
+		if (sd_mmc_get_type(slot) & CARD_TYPE_SDIO) {
+			// Test CIA of SDIO card
+			main_test_sdio(slot);
+		}
+		if (sd_mmc_get_type(slot) & (CARD_TYPE_SD | CARD_TYPE_MMC)) {
+			// SD/MMC Card R/W
+			main_test_memory(slot);
+		}
+
+		printf("Test finished, please unplugged the card.\n\r");
+		while (SD_MMC_OK == sd_mmc_check(slot)) {
+		}
+		slot++;
+	}
+}
+
+/**
+ * \brief Display basic information of the card.
+ * \note This function should be called only after the card has been
+ *       initialized successfully.
+ *
+ * \param slot   SD/MMC slot to test
+ */
+static void main_display_info_card(uint8_t slot)
+{
+	printf("Card information:\n\r");
+
+	printf("    ");
+	switch (sd_mmc_get_type(slot)) {
+	case CARD_TYPE_SD | CARD_TYPE_HC:
+		printf("SDHC");
+		break;
+	case CARD_TYPE_SD:
+		printf("SD");
+		break;
+	case CARD_TYPE_MMC | CARD_TYPE_HC:
+		printf("MMC High Density");
+		break;
+	case CARD_TYPE_MMC:
+		printf("MMC");
+		break;
+	case CARD_TYPE_SDIO:
+		printf("SDIO\n\r");
+		return;
+	case CARD_TYPE_SD_COMBO:
+		printf("SD COMBO");
+		break;
+	case CARD_TYPE_UNKNOWN:
+	default:
+		printf("Unknow\n\r");
+		return;
+	}
+	printf("\n\r    %d MB\n\r", (uint16_t)(sd_mmc_get_capacity(slot)/1024));
+}
+
+/**
+ * \brief Card R/W tests
+ *
+ * \param slot   SD/MMC slot to test
+ */
+static void main_test_memory(uint8_t slot)
+{
+	uint32_t last_blocks_addr, i, nb_trans;
+	uint32_t tick_start, time_ms;
+
+	// Compute the last address
+	last_blocks_addr = sd_mmc_get_capacity(slot) *
+			(1024 / SD_MMC_BLOCK_SIZE);
+	if (last_blocks_addr < (TEST_MEM_START_OFFSET / 512lu)) {
+		printf("[Memory is too small.]\n\r");
+		return;
+	}
+	last_blocks_addr -= (TEST_MEM_START_OFFSET / SD_MMC_BLOCK_SIZE);
+
+	printf("Card R/W test:\n\r");
+
+	// Read the last block
+	printf("    Read... ");
+	tick_start = 0;
+	if (SD_MMC_OK != sd_mmc_init_read_blocks(slot,
+			last_blocks_addr,
+			TEST_MEM_AREA_SIZE / SD_MMC_BLOCK_SIZE)) {
+		printf("[FAIL]\n\r");
+		return;
+	}
+	for (nb_trans = 0; nb_trans < (TEST_MEM_AREA_SIZE / TEST_MEM_ACCESS_SIZE);
+			nb_trans++) {
+		if (SD_MMC_OK != sd_mmc_start_read_blocks(buf_test,
+					TEST_MEM_ACCESS_SIZE / SD_MMC_BLOCK_SIZE)) {
+			printf("[FAIL]\n\r");
+			return;
+		}
+		if (SD_MMC_OK != sd_mmc_wait_end_of_read_blocks(false)) {
+			printf("[FAIL]\n\r");
+			return;
+		}		
+	}
+	time_ms = 0;
+	if (time_ms) { // Valid time_ms
+		printf(" %d KBps ", (int)(((TEST_MEM_AREA_SIZE
+				* 1000lu) / 1024lu) / time_ms));
+	}
+	printf("[OK]\n\r");
+
+	if (sd_mmc_is_write_protected(slot)) {
+		printf("Card is write protected [WRITE TEST SKIPPED]\n\r");
+		return;
+	}
+	
+	// Fill buffer with a pattern
+	for (i = 0; i < (TEST_MEM_ACCESS_SIZE / sizeof(uint32_t)); i++) {
+		((uint32_t*)buf_test)[i] = TEST_FILL_VALUE_U32;
 	}
 
-	while (1) {
-		/* Do nothing */
+	printf("    Write pattern... ");
+	if (SD_MMC_OK != sd_mmc_init_write_blocks(slot,
+			last_blocks_addr,
+			TEST_MEM_AREA_SIZE / SD_MMC_BLOCK_SIZE)) {
+		printf("[FAIL]\n\r");
+		return;
 	}
+	tick_start = 0;
+	for (nb_trans = 0; nb_trans < (TEST_MEM_AREA_SIZE / TEST_MEM_ACCESS_SIZE);
+			nb_trans++) {
+		((uint32_t*)buf_test)[0] = nb_trans; // Unique value for each area
+		if (SD_MMC_OK != sd_mmc_start_write_blocks(buf_test,
+				TEST_MEM_ACCESS_SIZE / SD_MMC_BLOCK_SIZE)) {
+			printf("[FAIL]\n\r");
+			return;
+		}
+		if (SD_MMC_OK != sd_mmc_wait_end_of_write_blocks(false)) {
+			printf("[FAIL]\n\r");
+			return;
+		}
+	}
+	time_ms = 0;
+	if (time_ms) { // Valid time_ms
+		printf(" %d KBps ", (int)(((TEST_MEM_AREA_SIZE
+				* 1000lu) / 1024lu) / time_ms));
+	}
+	printf("[OK]\n\r");
+
+	printf("    Read and check pattern... ");
+	if (SD_MMC_OK != sd_mmc_init_read_blocks(slot,
+			last_blocks_addr,
+			TEST_MEM_AREA_SIZE / SD_MMC_BLOCK_SIZE)) {
+		printf("Read [FAIL]\n\r");
+		return;
+	}
+	for (nb_trans = 0; nb_trans < (TEST_MEM_AREA_SIZE / TEST_MEM_ACCESS_SIZE);
+			nb_trans++) {
+		// Clear buffer
+		for (i = 0; i < (TEST_MEM_ACCESS_SIZE / sizeof(uint32_t)); i++) {
+			((uint32_t*)buf_test)[i] = 0xFFFFFFFF;
+		}
+		// Fill buffer
+		if (SD_MMC_OK != sd_mmc_start_read_blocks(buf_test,
+					TEST_MEM_ACCESS_SIZE / SD_MMC_BLOCK_SIZE)) {
+			printf("Read [FAIL]\n\r");
+			return;
+		}
+		if (SD_MMC_OK != sd_mmc_wait_end_of_read_blocks(false)) {
+			printf("Read [FAIL]\n\r");
+			return;
+		}
+		// Check the unique value of the area
+		if (((uint32_t*)buf_test)[0] != nb_trans) {
+			printf("Check [FAIL]\n\r");
+			return;
+		}
+		// Check buffer
+		for (i = 1; i < (TEST_MEM_ACCESS_SIZE / sizeof(uint32_t)); i++) {
+			if (((uint32_t*)buf_test)[i] != TEST_FILL_VALUE_U32) {
+				printf("Check [FAIL]\n\r");
+				return;
+			}
+		}
+	}
+	printf("[OK]\n\r");
+}
+
+/**
+ * \brief Perform test on CIA (Common I/O Area) of SDIO card.
+ *
+ * \note The Common I/O Area (CIA) shall be implemented on all SDIO cards.
+ *
+ * \param slot   SD/MMC slot to test
+ */
+static void main_test_sdio(uint8_t slot)
+{
+	uint32_t i;
+	uint32_t err;
+
+	/*
+	 * Test with direct read and write command.
+	 */
+	printf("\n\r--- Test with direct read and write command of CIA:\n\r");
+	/* Read */
+	for (i = 0; i < TEST_CIA_SIZE; i++) {
+		err = sdio_read_direct(slot, SDIO_CIA, i, &buf_cia[i]);
+		if (err) {
+			printf("Error: SDIO direct read failed.\n\r");
+			return;
+		}
+	}
+	main_dump_buffer(buf_cia, TEST_CIA_SIZE);
+
+	/* Write */
+	printf("Write 0x02 to IEN(CIA.4).\n\r");
+	err = sdio_write_direct(slot, SDIO_CIA, SDIO_CCCR_IEN, 0x02);
+	if (err) {
+		printf("Error: SDIO direct write failed.\n\r");
+		return;
+	}
+
+	/* Check */
+	printf("Check IEN after write:");
+	err = sdio_read_direct(slot, SDIO_CIA, SDIO_CCCR_IEN,
+			&buf_cia[SDIO_CCCR_IEN]);
+	if (err) {
+		printf("Error: SDIO direct read failed.\n\r");
+		return;
+	}
+
+	printf("0x%02x\n\r", buf_cia[SDIO_CCCR_IEN]);
+	if (0x02 == buf_cia[SDIO_CCCR_IEN]) {
+		printf("Test OK.\n\r");
+	} else {
+		printf("Test failed.\n\r");
+	}
+
+	/* Restore data to 0 */
+	sdio_write_direct(slot, SDIO_CIA, SDIO_CCCR_IEN, 0);
+
+	/* Clear the buffer. */
+	memset(buf_cia, 0x0, sizeof(buf_cia));
+
+	/*
+	 *  Test with extended read and write command.
+	 */
+	printf("\n\r--- Test with extended read and write command of CIA:\n\r");
+	/* Read */
+	err = sdio_read_extended(slot, SDIO_CIA, 0, 1, &buf_cia[0],
+			TEST_CIA_SIZE);
+	if (err) {
+		printf("Error: SDIO extended read failed.\n\r");
+		return;
+	}
+
+	main_dump_buffer(buf_cia, TEST_CIA_SIZE);
+
+	/* Write */
+	printf("Modify Some R/W bytes for FN0 and write:\n\r");
+	buf_cia[SDIO_CCCR_IEN] = 0x3;
+	err = sdio_write_extended(slot, SDIO_CIA, SDIO_CCCR_IEN, 1,
+			&buf_cia[SDIO_CCCR_IEN], 1);
+	if (err) {
+		printf("Error: SDIO extended write failed.\n\r");
+		return;
+	}
+
+	/* Read and check */
+	printf("Check CIA after write:\n\r");
+	err = sdio_read_extended(slot, SDIO_CIA, 0, 1, &buf_cia[0],
+			TEST_CIA_SIZE);
+	if (err) {
+		printf("Error: SDIO extended read failed.\n\r");
+		return;
+	}
+
+	main_dump_buffer(buf_cia, TEST_CIA_SIZE);
+
+	if (buf_cia[SDIO_CCCR_IEN] != 0x3) {
+		printf("CIA.4 Fail\n\r");
+	} else {
+		printf("Test OK\n\r");
+	}
+
+	/* Restore data to 0 */
+	sdio_write_direct(slot, SDIO_CIA, SDIO_CCCR_IEN, 0);
+
+	return;
+}
+
+/**
+ * \brief Dump and print buffer.
+ *
+ * \param data_buffer Pointer to data buffer.
+ * \param length      Buffer length.
+ */
+static void main_dump_buffer(uint8_t *data_buffer, uint32_t length)
+{
+	uint32_t i;
+
+	printf("Dump buffer (length=%d):", (int)length);
+	for (i = 0; i < length; i++) {
+		if ((i % 16) == 0) { /* Display 16 data per line */
+			printf("\n\r%3x:", (int)i);
+		}
+
+		printf(" %02x", data_buffer[i]);
+	}
+	printf("\n\r");
 }

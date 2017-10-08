c68da305272ac805419106530768637361874285
Art Navsegda <artnavsegda@gmail.com>
2017-10-08 17:26:39 +0300

    1

--- atmelstudio7/xmega/sdcard/sdcard/src/main.c
+++ atmelstudio7/xmega/sdcard/sdcard/src/main.c
@@ -1,4 +1,5 @@
 #include <asf.h>
+#include <string.h>
 
 const usart_serial_options_t usart_serial_options = {
 	.baudrate     = 9600,
@@ -9,6 +10,11 @@ const usart_serial_options_t usart_serial_options = {
 
 int main (void)
 {
+	char test_file_name[] = "0:sd_mmc_test.txt";
+	Ctrl_status status;
+	FRESULT res;
+	FATFS fs;
+	FIL file_object;
 	/* Insert system clock initialization code here (sysclk_init()). */
 
 	sysclk_init();
@@ -19,4 +25,55 @@ int main (void)
 	printf("sd mmc start\n\r");
 
 	/* Insert application code here, after the board has been initialized. */
+	printf("\x0C\n\r-- SD/MMC/SDIO Card Example on FatFs --\n\r");
+	printf("-- Compiled: %s %s --\n\r", __DATE__, __TIME__);
+	while (1) {
+		printf("Please plug an SD, MMC or SDIO card in slot.\n\r");
+
+		/* Wait card present and ready */
+		do {
+			status = sd_mmc_test_unit_ready(0);
+			if (CTRL_FAIL == status) {
+				printf("Card install FAIL\n\r");
+				printf("Please unplug and re-plug the card.\n\r");
+				while (CTRL_NO_PRESENT != sd_mmc_check(0)) {
+				}
+			}
+		} while (CTRL_GOOD != status);
+
+		printf("Mount disk (f_mount)...\r\n");
+		memset(&fs, 0, sizeof(FATFS));
+		res = f_mount(LUN_ID_SD_MMC_0_MEM, &fs);
+		if (FR_INVALID_DRIVE == res) {
+			printf("[FAIL] res %d\r\n", res);
+			goto main_end_of_test;
+		}
+		printf("[OK]\r\n");
+
+		printf("Create a file (f_open)...\r\n");
+		test_file_name[0] = LUN_ID_SD_MMC_0_MEM + '0';
+		res = f_open(&file_object,
+		(char const *)test_file_name,
+		FA_CREATE_ALWAYS | FA_WRITE);
+		if (res != FR_OK) {
+			printf("[FAIL] res %d\r\n", res);
+			goto main_end_of_test;
+		}
+		printf("[OK]\r\n");
+
+		printf("Write to test file (f_puts)...\r\n");
+		if (0 == f_puts("Test SD/MMC stack\n", &file_object)) {
+			f_close(&file_object);
+			printf("[FAIL]\r\n");
+			goto main_end_of_test;
+		}
+		printf("[OK]\r\n");
+		f_close(&file_object);
+		printf("Test is successful.\n\r");
+
+		main_end_of_test:
+		printf("Please unplug the card.\n\r");
+		while (CTRL_NO_PRESENT != sd_mmc_check(0)) {
+		}
+	}
 }

b2fef3c2713e5c86d89df3d5f682f5684bf24401
Art Navsegda <artnavsegda@gmail.com>
2017-10-12 11:58:42 +0300

    zmega timer

--- eclipse/xmega/xmega_timer/main.c
+++ eclipse/xmega/xmega_timer/main.c
@@ -3,14 +3,14 @@
 
 ISR(TCC0_OVF_vect)
 {
-	PORTD_OUTTGL = _BV(4);
+	PORTD_OUTTGL = _BV(3);
 }
 
 int main(void)
 {
-	PORTD_DIRSET = _BV(4);
-	TCC0_PER = 0x2710;                // Set period 10000
-	TCC0_CTRLA = 0x06;                // Prescaler DIV64
+	PORTD_DIRSET = _BV(3);
+	TCC0_PER = 20000;                // Set period 10000
+	TCC0_CTRLA = TC_CLKSEL_DIV1_gc;                // Prescaler DIV1
 	TCC0_INTCTRLA = 2;                // Enable overflow interrupt
 	PMIC_CTRL = 2;                    // Enable medium level interrupts
 	sei();

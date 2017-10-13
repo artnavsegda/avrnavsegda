c28065700388bbe9c15c171b2a44422925704945
Art Navsegda <artnavsegda@gmail.com>
2017-10-13 14:12:11 +0300

    fatfs

--- eclipse/xmega/xmega_sd-webserver/main.c
+++ eclipse/xmega/xmega_sd-webserver/main.c
@@ -83,13 +83,13 @@ uint8_t predefined_set_cgi_processor(uint8_t * uri_name, uint8_t * uri, uint8_t 
 void start_ethernet(void)
 {
 	wiz_NetInfo netinfo = { .mac 	= {0x00, 0x08, 0xdc, 0xab, 0xcd, 0xef},	// Mac address
-	                            .ip 	= {192, 168, 0, 112},					// IP address
-	                            .sn 	= {255, 255, 255, 0},					// Subnet mask
+	                            .ip 	= {192, 168, 1, 150},					// IP address
+	                            .sn 	= {255, 255, 0, 0},					// Subnet mask
 	                            .gw 	= {192, 168, 0, 1},						// Gateway address
 	                            .dns 	= {8, 8, 8, 8},							// DNS server
 	                            .dhcp 	= NETINFO_STATIC };						// DHCP enable / disable
 
-	uint8_t memsize[2][8] = { { 2, 2, 2, 2, 2, 2, 2, 2 }, { 2, 2, 2, 2, 2, 2, 2, 2 } };
+	uint8_t memsize[2][8] = { { 2, 2, 2, 2 }, { 2, 2, 2, 2 } };
 	reg_wizchip_cs_cbfunc(wizchip_select, wizchip_deselect);
 	reg_wizchip_spi_cbfunc(wizchip_read, wizchip_write);
 	reg_wizchip_spiburst_cbfunc(wizchip_readburst, wizchip_writeburst);
@@ -107,13 +107,10 @@ void start_timer(void)
 	sei();
 }
 
-void start_sd(void)
-{
-	disk_initialize(0);
-}
-
 int main(void)
 {
+	FATFS fs;
+
 	start_timer();
 	start_serial();
 	printf("Serial started\r\n");
@@ -135,8 +132,14 @@ int main(void)
 	// disable wiznet spi
 	PORTB.OUTCLR = PIN7_bm;
 
+	// MISO level pull up
+	PORTC.PIN6CTRL = PORT_OPC_PULLUP_gc;
 	// Enable SPI, Master Mode 0, set the clock rate fck/2
 	SPIC.CTRL = SPI_ENABLE_bm | SPI_MASTER_bm;
+
+	// Reset and check sd card
+	f_mount(&fs, "", 0);
+	printf("SD card started\r\n");
 
 	// Reset W5100
 	PORTA.OUTCLR = PIN0_bm; //write zero
@@ -145,7 +148,6 @@ int main(void)
 
 	start_ethernet();
 	printf("Ethernet started\r\n");
-	start_sd();
 	httpServer_init(TX_BUF, RX_BUF, MAX_HTTPSOCK, socknumlist);
 
 	while(1)

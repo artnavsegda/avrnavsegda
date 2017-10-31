a09b7dcbdbbd5be568d16ebe6450d72c183ea0bd
Art Navsegda <artnavsegda@gmail.com>
2017-10-31 23:40:14 +0300

    vsratenko

--- eclipse/xmega/xmega_pca9557/main.c
+++ eclipse/xmega/xmega_pca9557/main.c
@@ -13,6 +13,31 @@ void Expander_Init(char ModuleAddress)
         Expander_Write_Byte(ModuleAddress,0x02,0x00);
 }
 
+void TWIE_Init(int twibaud)
+{
+	TWIE.MASTER.CTRLB = TWI_MASTER_SMEN_bm;
+	TWIE.MASTER.BAUD = TWI_BAUD(F_CPU, twibaud);
+	TWIE.MASTER.CTRLA = TWI_MASTER_ENABLE_bm;
+	TWIE.MASTER.STATUS = TWI_MASTER_BUSSTATE_IDLE_gc;
+}
+
+char TWIE_Read(char ModuleAddress)
+{
+	TWIE.MASTER.ADDR = ModuleAddress<<1;
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
+	TWIE.MASTER.DATA = 0x00;//write word addr
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
+	TWIE.MASTER.ADDR = (ModuleAddress<<1)+1;
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_RIF_bp);
+	TWIE.MASTER.CTRLC = TWI_MASTER_ACKACT_bm;
+	return TWIE.MASTER.DATA;
+}
+
+void Expander_Write_Byte(char ModuleAddress, char Register, char WriteData)
+{
+
+}
+
 int main(void)
 {
 	startserial();

da420566e56aeff98256744daccef973aeebbeb5
Art Navsegda <artnavsegda@gmail.com>
2017-10-31 18:33:26 +0300

    changes

--- eclipse/xmega/xmega_wiznet-webserver-sd/main.c
+++ eclipse/xmega/xmega_wiznet-webserver-sd/main.c
@@ -14,12 +14,16 @@ uint8_t TX_BUF[DATA_BUF_SIZE];
 #define MAX_HTTPSOCK	2
 uint8_t socknumlist[] = {2, 3};
 
-volatile UINT Timer;    /* Performance timer (100Hz increment) */
-
 ISR(TCC0_OVF_vect)
 {
-	Timer++;                        /* Performance counter for this module */
 	disk_timerproc();       /* Drive timer procedure of low level disk I/O module */
+	PORTD.OUTTGL = PIN4_bm;
+}
+
+ISR(TCC1_OVF_vect)
+{
+	httpServer_time_handler();
+	PORTD.OUTTGL = PIN5_bm;
 }
 
 static void  wizchip_select(void)
@@ -89,7 +93,7 @@ void start_ethernet(void)
 	                            .dns 	= {8, 8, 8, 8},							// DNS server
 	                            .dhcp 	= NETINFO_STATIC };						// DHCP enable / disable
 
-	uint8_t memsize[2][8] = { { 2, 2, 2, 2 }, { 2, 2, 2, 2 } };
+	uint8_t memsize[2][4] = { { 2, 2, 2, 2 }, { 2, 2, 2, 2 } };
 	reg_wizchip_cs_cbfunc(wizchip_select, wizchip_deselect);
 	reg_wizchip_spi_cbfunc(wizchip_read, wizchip_write);
 	reg_wizchip_spiburst_cbfunc(wizchip_readburst, wizchip_writeburst);
@@ -102,18 +106,34 @@ void start_timer(void)
 	TCC0_PER = 20000;                // Set period 10000
 	TCC0_CTRLA = TC_CLKSEL_DIV1_gc;                // Prescaler DIV1
 	TCC0_INTCTRLA = 2;                // Enable overflow interrupt
+	// 31250 * 64 = 2000000 = 2 mHz
+	TCC1_PER = 31250;                // Set period 31250
+	TCC1_CTRLA = TC_CLKSEL_DIV64_gc;                // Prescaler DIV64
+	TCC1_INTCTRLA = 2;                // Enable overflow interrupt
 	PMIC_CTRL = 2;                    // Enable medium level interrupts
-
 	sei();
+}
+
+void Display_Net_Conf()
+{
+	wiz_NetInfo gWIZNETINFO;
+	ctlnetwork(CN_GET_NETINFO, (void*) &gWIZNETINFO);
+	printf("\r\nMAC: %02X:%02X:%02X:%02X:%02X:%02X\r\n", gWIZNETINFO.mac[0], gWIZNETINFO.mac[1], gWIZNETINFO.mac[2], gWIZNETINFO.mac[3], gWIZNETINFO.mac[4], gWIZNETINFO.mac[5]);
+	printf("IP: %d.%d.%d.%d\r\n", gWIZNETINFO.ip[0], gWIZNETINFO.ip[1], gWIZNETINFO.ip[2], gWIZNETINFO.ip[3]);
+	printf("GW: %d.%d.%d.%d\r\n", gWIZNETINFO.gw[0], gWIZNETINFO.gw[1], gWIZNETINFO.gw[2], gWIZNETINFO.gw[3]);
+	printf("SN: %d.%d.%d.%d\r\n", gWIZNETINFO.sn[0], gWIZNETINFO.sn[1], gWIZNETINFO.sn[2], gWIZNETINFO.sn[3]);
+	printf("DNS: %d.%d.%d.%d\r\n", gWIZNETINFO.dns[0], gWIZNETINFO.dns[1], gWIZNETINFO.dns[2], gWIZNETINFO.dns[3]);
 }
 
 int main(void)
 {
 	FATFS fs;
+	uint8_t tmpstr[6] = {0,};
 
-	start_timer();
 	start_serial();
 	printf("Serial started\r\n");
+	start_timer();
+	printf("Timer started\r\n");
 
 	// Initial the Peripheral
 	// Set MOSI (PORTC5),SCK (PORTC7), PORTC0 (ETH SS) and PORTC4 (adc SS) as output, others as input
@@ -124,6 +144,8 @@ int main(void)
 	PORTA.DIRSET = PIN0_bm;
 	// SEN wiznet
 	PORTB.DIRSET = PIN7_bm;
+	// status/power LED
+	PORTD.DIRSET = PIN4_bm | PIN5_bm;
 
 	// adc SS and eth SS deactivate
 	PORTC.OUTSET = PIN4_bm|PIN0_bm;
@@ -147,7 +169,9 @@ int main(void)
 	PORTA.OUTSET = PIN0_bm; //write one
 
 	start_ethernet();
-	printf("Ethernet started\r\n");
+	ctlwizchip(CW_GET_ID,(void*)tmpstr);
+	printf("Wiznet %s started\r\n", tmpstr);
+	Display_Net_Conf();
 	httpServer_init(TX_BUF, RX_BUF, MAX_HTTPSOCK, socknumlist);
 
 	while(1)

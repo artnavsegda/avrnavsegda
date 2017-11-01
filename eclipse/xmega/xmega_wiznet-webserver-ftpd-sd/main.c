8035bd7d2dee837ef959134555136a9d1923c44e
Art Navsegda <artnavsegda@gmail.com>
2017-11-01 22:58:57 +0300

    memory leak

--- eclipse/xmega/xmega_wiznet-webserver-ftpd-sd/main.c
+++ eclipse/xmega/xmega_wiznet-webserver-ftpd-sd/main.c
@@ -9,7 +9,7 @@
 #include "ftpd.h"
 #include "httpServer.h"
 
-uint8_t gFTPBUF[4096];
+uint8_t gFTPBUF[1024];
 uint8_t RX_BUF[1024];
 uint8_t TX_BUF[1024];
 uint8_t socknumlist[] = {2, 3};
@@ -102,6 +102,10 @@ void start_timer(void)
 	TCC0_PER = 20000;                // Set period 10000
 	TCC0_CTRLA = TC_CLKSEL_DIV1_gc;                // Prescaler DIV1
 	TCC0_INTCTRLA = 2;                // Enable overflow interrupt
+	// 31250 * 64 = 2000000 = 2 mHz
+	TCC1_PER = 31250;                // Set period 31250
+	TCC1_CTRLA = TC_CLKSEL_DIV64_gc;                // Prescaler DIV64
+	TCC1_INTCTRLA = 2;                // Enable overflow interrupt
 	PMIC_CTRL = 2;                    // Enable medium level interrupts
 	sei();
 }

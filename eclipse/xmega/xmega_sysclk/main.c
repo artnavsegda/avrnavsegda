705d020f543d4f3c36da84780117da3ffc01960b
Art Navsegda <artnavsegda@gmail.com>
2017-11-03 01:14:29 +0300

    kaaay

--- eclipse/xmega/xmega_sysclk/main.c
+++ eclipse/xmega/xmega_sysclk/main.c
@@ -1,4 +1,12 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
 int main(void)
 {
+    OSC_CTRL = 0x02;
+    while(OSC_STATUS != OSC_RC32MRDY_bm);
+    CPU_CCP = 0xD8;
+    CLK_CTRL = 1;
+
 	while(1);
 }

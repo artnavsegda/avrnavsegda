5bf25f13970b943177e7aae183376e3d5ad62dff
Art Navsegda <artnavsegda@gmail.com>
2017-11-03 01:01:50 +0300

    gazebo

--- eclipse/xmega/xmega_sysclk-avr1003/main.c
+++ eclipse/xmega/xmega_sysclk-avr1003/main.c
@@ -1,4 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "avr_compiler.h"
+#include "clksys_driver.h"
+
 int main(void)
 {
+	CLKSYS_Enable( OSC_RC32MEN_bm );
+	while (!CLKSYS_IsReady(OSC_RC32MRDY_bm ));
+	CLKSYS_Main_ClockSource_Select(CLK_SCLKSEL_RC32M_gc);
+	CLKSYS_Disable(OSC_RC2MEN_bm);
+
 	while(1);
 }

bc468dad5b2c9edff7ef2550822b4d8b2120015c
Art Navsegda <artnavsegda@gmail.com>
2017-11-02 23:35:34 +0300

    adc staging

--- eclipse/xmega/xmega_adc/main.c
+++ eclipse/xmega/xmega_adc/main.c
@@ -6,11 +6,19 @@
 
 int main(void)
 {
+	int16_t result = 0;
 	startserial();
 	printf("MCU started\n\r");
+	ADCA.CTRLB = ADC_RESOLUTION_12BIT_gc;
+	ADCA.PRESCALER = ADC_PRESCALER_DIV4_gc;
+	ADCA.REFCTRL = ADC_REFSEL_INTVCC_gc;
+	ADCA.CH0.CTRL = ADC_CH_INPUTMODE_SINGLEENDED_gc;
+
 	while(1)
 	{
-
+		ADCA.CH0.CTRL = ADC_CH_START_bm;
+		while(!ADCA.CH0.INTFLAGS);
+		result = ADCA.CH0RES;
 	}
 	return 0;
 }

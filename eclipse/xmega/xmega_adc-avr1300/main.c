84a49ab86e3fbbf3f0441c4e7314898732acea6d
Art Navsegda <artnavsegda@gmail.com>
2017-11-02 22:45:29 +0300

    blyat

--- eclipse/xmega/xmega_adc-avr1300/main.c
+++ eclipse/xmega/xmega_adc-avr1300/main.c
@@ -5,8 +5,19 @@
 
 int main(void)
 {
+	int16_t result = 0;
 	ADC_ConvMode_and_Resolution_Config(&ADCA, ADC_ConvMode_Signed, ADC_RESOLUTION_12BIT_gc);
 	ADC_Prescaler_Config(&ADCA, ADC_PRESCALER_DIV32_gc);
 	ADC_Reference_Config(&ADCA, ADC_REFSEL_INTVCC_gc);
 	ADC_Ch_InputMode_and_Gain_Config(&ADCA.CH0, ADC_CH_INPUTMODE_SINGLEENDED_gc, ADC_DRIVER_CH_GAIN_NONE);
+	ADC_Ch_InputMux_Config(&ADCA.CH3, ADC_CH_MUXPOS_PIN4_gc, 0);
+	ADC_Enable(&ADCA);
+
+	while(1)
+	{
+		ADC_Ch_Conversion_Start(&ADCA.CH3);
+		while(!ADC_Ch_Conversion_Complete(&ADCA.CH3));
+		result = ADC_ResultCh_GetWord_Signed(&ADCA.CH3, 0);
+	}
+
 }

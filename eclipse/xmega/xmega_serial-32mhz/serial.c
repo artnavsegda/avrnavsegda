f22ef3e387ce76ce0814b5f860c3b4e4f8dda878
Art Navsegda <artnavsegda@gmail.com>
2017-11-03 03:07:02 +0300

    serial 32 mhz

---
+++ eclipse/xmega/xmega_serial-32mhz/serial.c
@@ -0,0 +1,42 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+static int uart_putchar(char c, FILE *stream);
+
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+static int uart_putchar(char c, FILE *stream)
+{
+	loop_until_bit_is_set(USARTC0.STATUS,USART_DREIF_bp);
+	USARTC0.DATA = c;
+	return 0;
+}
+
+int main(void)
+{
+    OSC.CTRL = 0x02;
+    while(!(OSC.STATUS & OSC_RC32MRDY_bm));
+    CPU_CCP = 0xD8;
+    CLK.CTRL = 1;
+
+	PORTC.DIRSET = PIN3_bm;
+	PORTC.DIRCLR = PIN2_bm;
+
+	USARTC0.CTRLC = (uint8_t) USART_CHSIZE_8BIT_gc | USART_PMODE_DISABLED_gc | false;
+	USARTC0.BAUDCTRLA = 207;//9600
+
+	USARTC0.CTRLB |= USART_RXEN_bm;
+	USARTC0.CTRLB |= USART_TXEN_bm;
+
+	stdout = &mystdout;
+
+	printf("Hello eclipse World!\n\r");
+
+    while(1)
+    {
+    		_delay_ms(1000);
+        printf("hello\n\r");
+    }
+}

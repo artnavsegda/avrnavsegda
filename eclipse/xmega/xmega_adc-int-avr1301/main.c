1a9b8dcdcfab63ae9a72076aa28b814b8d48f5ae
Art Navsegda <artnavsegda@gmail.com>
2017-11-03 05:00:04 +0300

    some fixes

--- eclipse/xmega/xmega_adc-int-avr1301/main.c
+++ eclipse/xmega/xmega_adc-int-avr1301/main.c
@@ -5,9 +5,16 @@
 #include "adc_driver.h"
 #include "serial.h"
 
+int16_t result = 0;
+
+ISR(ADCA_CH3_vect)
+{
+	result = ADC_ResultCh_GetWord_Signed(&ADCA.CH3, 0);
+	ADC_Ch_Conversion_Start(&ADCA.CH3);
+}
+
 int main(void)
 {
-	int16_t result = 0;
 	startserial();
 	printf("MCU started\n\r");
 	ADC_ConvMode_and_Resolution_Config(&ADCA, ADC_ConvMode_Signed, ADC_RESOLUTION_12BIT_gc);
@@ -15,15 +22,23 @@ int main(void)
 	ADC_Reference_Config(&ADCA, ADC_REFSEL_INTVCC_gc);
 	ADC_Ch_InputMode_and_Gain_Config(&ADCA.CH0, ADC_CH_INPUTMODE_SINGLEENDED_gc, ADC_DRIVER_CH_GAIN_NONE);
 	ADC_Ch_InputMux_Config(&ADCA.CH3, ADC_CH_MUXPOS_PIN4_gc, 0);
+
+	ADC_Ch_Interrupts_Config(&ADCA.CH3, ADC_CH_INTMODE_COMPLETE_gc, ADC_CH_INTLVL_LO_gc);
+	PMIC.CTRL |= PMIC_LOLVLEX_bm;
+	sei();
+
 	ADC_Enable(&ADCA);
+	ADC_Ch_Conversion_Start(&ADCA.CH3);
+
+	//ADC_FreeRunning_Enable(&ADCA);
 
 	while(1)
 	{
-		ADC_Ch_Conversion_Start(&ADCA.CH3);
-		while(!ADC_Ch_Conversion_Complete(&ADCA.CH3));
-		result = ADC_ResultCh_GetWord_Signed(&ADCA.CH3, 0);
+		//ADC_Ch_Conversion_Start(&ADCA.CH3);
+		//while(!ADC_Ch_Conversion_Complete(&ADCA.CH3));
+		//result = ADC_ResultCh_GetWord_Signed(&ADCA.CH3, 0);
 		printf("adc: %d\n\r",result);
-		//_delay_ms(1000);
+		_delay_ms(1000);
 	}
 
 }
---
+++ eclipse/xmega/xmega_adc-int-avr1301/serial.h
@@ -0,0 +1,6 @@
+#ifndef SERIAL_H_
+#define SERIAL_H_
+
+void startserial(void);
+
+#endif /* SERIAL_H_ */

010fec3c9df9a8e059cb32b3934dd8c202aed627
Art Navsegda <artnavsegda@gmail.com>
2017-11-17 00:39:15 +0300

    ebanistika kakaya to

--- eclipse/xmega/xmega_pca9557/main.c
+++ eclipse/xmega/xmega_pca9557/main.c
@@ -2,12 +2,28 @@
 #include <util/delay.h>
 #include <stdio.h>
 #include "serial.h"
-#include "i2c.h"
+
+#define TWI_BAUD(F_SYS, F_TWI) ((F_SYS / (2 * F_TWI)) - 5)
+
+void starti2c(void)
+{
+	TWIE.MASTER.CTRLB = TWI_MASTER_SMEN_bm;
+	TWIE.MASTER.BAUD = TWI_BAUD(2000000, 10000);
+	TWIE.MASTER.CTRLA = TWI_MASTER_ENABLE_bm;
+	TWIE.MASTER.STATUS = TWI_MASTER_BUSSTATE_IDLE_gc;
+}
 
 uint8_t pcaread(uint8_t ModuleAddress, uint8_t ModuleRegister)
 {
-	i2csend(0x18, ModuleRegister);//write word addr
-	uint8_t RegisterData = i2cread(0x18);
+	TWIE.MASTER.ADDR = ModuleAddress<<1;
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
+	TWIE.MASTER.DATA = ModuleRegister;//write word addr
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
+
+	TWIE.MASTER.ADDR = (ModuleAddress<<1)+1;
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_RIF_bp);
+	uint8_t RegisterData = TWIE.MASTER.DATA;
+
 	TWIE.MASTER.CTRLC = TWI_MASTER_ACKACT_bm;
 	return RegisterData;
 }

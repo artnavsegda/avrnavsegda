571c4ea0c8e953c6da793d92ad8cd05bb1d236e1
Art Navsegda <artnavsegda@gmail.com>
2017-11-17 00:17:16 +0300

    all shit

--- eclipse/xmega/xmega_i2c/main.c
+++ eclipse/xmega/xmega_i2c/main.c
@@ -5,25 +5,37 @@
 
 #define TWI_BAUD(F_SYS, F_TWI) ((F_SYS / (2 * F_TWI)) - 5)
 
-char ModuleAddress = 0x18;
-
-int main(void)
+void starti2c(void)
 {
-	startserial();
-	//twi init
 	TWIE.MASTER.CTRLB = TWI_MASTER_SMEN_bm;
 	TWIE.MASTER.BAUD = TWI_BAUD(2000000, 10000);
 	TWIE.MASTER.CTRLA = TWI_MASTER_ENABLE_bm;
 	TWIE.MASTER.STATUS = TWI_MASTER_BUSSTATE_IDLE_gc;
+}
+
+void i2csend(uint8_t ModuleAddress, uint8_t bytetosend)
+{
+	TWIE.MASTER.ADDR = ModuleAddress<<1;
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
+	TWIE.MASTER.DATA = bytetosend;//write word addr
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
+}
+
+uint8_t i2cread(uint8_t ModuleAddress)
+{
+	TWIE.MASTER.ADDR = (ModuleAddress<<1)+1;
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_RIF_bp);
+	return TWIE.MASTER.DATA;
+}
+
+int main(void)
+{
+	startserial();
+	starti2c();
 	while (1)
 	{
-		TWIE.MASTER.ADDR = ModuleAddress<<1;
-		loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
-		TWIE.MASTER.DATA = 0x00;//write word addr
-		loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
-		TWIE.MASTER.ADDR = (ModuleAddress<<1)+1;
-		loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_RIF_bp);
-		printf("Data: %x\r\n", TWIE.MASTER.DATA);
+		i2csend(0x18, 0x00);//write word addr
+		printf("Data: %x\r\n", i2cread(0x18));
 		TWIE.MASTER.CTRLC = TWI_MASTER_ACKACT_bm;
 		_delay_ms(1000);
 	}
--- eclipse/xmega/xmega_pca9557/main.c
+++ eclipse/xmega/xmega_pca9557/main.c
@@ -5,56 +5,37 @@
 
 #define TWI_BAUD(F_SYS, F_TWI) ((F_SYS / (2 * F_TWI)) - 5)
 
-char ModuleAddress = 0x18;
-
-void Expander_Init(char ModuleAddress)
-{
-        TWIE_Init(100000);
-        Expander_Write_Byte(ModuleAddress,0x02,0x00);
-}
-
-void TWIE_Init(int twibaud)
+void starti2c(void)
 {
 	TWIE.MASTER.CTRLB = TWI_MASTER_SMEN_bm;
-	TWIE.MASTER.BAUD = TWI_BAUD(F_CPU, twibaud);
+	TWIE.MASTER.BAUD = TWI_BAUD(2000000, 10000);
 	TWIE.MASTER.CTRLA = TWI_MASTER_ENABLE_bm;
 	TWIE.MASTER.STATUS = TWI_MASTER_BUSSTATE_IDLE_gc;
 }
 
-char TWIE_Read(char ModuleAddress)
+void i2csend(uint8_t ModuleAddress, uint8_t bytetosend)
 {
 	TWIE.MASTER.ADDR = ModuleAddress<<1;
 	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
-	TWIE.MASTER.DATA = 0x00;//write word addr
+	TWIE.MASTER.DATA = bytetosend;//write word addr
 	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
-	TWIE.MASTER.ADDR = (ModuleAddress<<1)+1;
-	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_RIF_bp);
-	TWIE.MASTER.CTRLC = TWI_MASTER_ACKACT_bm;
-	return TWIE.MASTER.DATA;
 }
 
-void Expander_Write_Byte(char ModuleAddress, char Register, char WriteData)
+uint8_t i2cread(uint8_t ModuleAddress)
 {
-
+	TWIE.MASTER.ADDR = (ModuleAddress<<1)+1;
+	loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_RIF_bp);
+	return TWIE.MASTER.DATA;
 }
 
 int main(void)
 {
 	startserial();
-	//twi init
-	TWIE.MASTER.CTRLB = TWI_MASTER_SMEN_bm;
-	TWIE.MASTER.BAUD = TWI_BAUD(2000000, 10000);
-	TWIE.MASTER.CTRLA = TWI_MASTER_ENABLE_bm;
-	TWIE.MASTER.STATUS = TWI_MASTER_BUSSTATE_IDLE_gc;
+	starti2c();
 	while (1)
 	{
-		TWIE.MASTER.ADDR = ModuleAddress<<1;
-		loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
-		TWIE.MASTER.DATA = 0x00;//write word addr
-		loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_WIF_bp);
-		TWIE.MASTER.ADDR = (ModuleAddress<<1)+1;
-		loop_until_bit_is_set(TWIE.MASTER.STATUS, TWI_MASTER_RIF_bp);
-		printf("Data: %x\r\n", TWIE.MASTER.DATA);
+		i2csend(0x18, 0x00);//write word addr
+		printf("Data: %x\r\n", i2cread(0x18));
 		TWIE.MASTER.CTRLC = TWI_MASTER_ACKACT_bm;
 		_delay_ms(1000);
 	}

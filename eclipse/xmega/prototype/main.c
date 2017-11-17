d75aa1bd929aacce143d639b3f685da347c703a0
Art Navsegda <artnavsegda@gmail.com>
2017-11-17 20:42:35 +0300

    govennnnn

--- eclipse/xmega/prototype/main.c
+++ eclipse/xmega/prototype/main.c
@@ -12,6 +12,13 @@ int main(void)
 	pcawrite(0x18,0x02,0x00);//reset inversion
 	pcawrite(0x19,0x02,0x00);//reset inversion
 	pcawrite(0x1a,0x02,0x00);//reset inversion
+
+	pcawrite(0x1a,0x03,!_BV(1));//set ignition as output
+	pcawrite(0x1a,0x01,_BV(1));//enable ignition
+	_delay_ms(1000);
+	pcawrite(0x1a,0x01,!_BV(1));//enable ignition
+
+
 }
 
 

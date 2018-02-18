c4d6f109f56a8b55103d2858cdd0a09a706ffa6f
Art Navsegda <artnavsegda@gmail.com>
2018-02-18 21:03:54 +0300

    blink

--- manual/blink.c
+++ manual/blink.c
@@ -5,30 +5,27 @@
 
 int main(void)
 {
+	DDRC = 0xFF;
 	MCUCR = 0x80;
-	SFIOR = 0x78; //bus keeper enable
-	//SFIOR = 0x38; //bus keeper disable
+	//SFIOR = 0x78; //bus keeper enable
+	SFIOR = 0x38; //bus keeper disable
 
-	unsigned char * p = 0x500;
-	unsigned char * d = 0x5FF;
+	volatile unsigned char *p = (unsigned char *) 0x500;
+	volatile unsigned char *d = (unsigned char *) 0x5FF;
 	unsigned char x;
 
 	while (1)
 	{
-		*p = 0xFF;
-		_delay_ms(1000);
-		*d = 0xFF;
-		_delay_ms(1000);
-
 		*p = 0x00;
-		_delay_ms(1000);
-		*d = 0x00;
-		_delay_ms(1000);
-
-		*p = *d;
-		_delay_ms(1000);
-		*d = !*p;
-		_delay_ms(1000);
+		_delay_ms(100);
+		x = *p;
+		_delay_ms(100);
+		PORTC = ~x;
+		_delay_ms(100);	
+		x = *d;
+		_delay_ms(100);
+		PORTC = ~x;
+		_delay_ms(100);	
 	}
 	return 0;
 }

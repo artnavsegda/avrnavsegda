79e5bde5da531eaf4a87a1ad2174ed3007942c6d
Art Navsegda <artnavsegda@gmail.com>
2018-03-22 02:21:50 +0300

    interrupts

--- m8515/timer.c
+++ m8515/timer.c
@@ -2,14 +2,33 @@
 #define F_CPU 2000000
 #include <util/delay.h>
 
+#include <avr/interrupt.h>
+
+ISR(TIMER1_COMPA_vect)
+{
+	PORTD |= _BV(PD4);
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	PORTD &= ~_BV(PD4);
+}
+
 int main(void)
 {
-	DDRD |= _BV(PD5);
+	DDRD |= _BV(PD5)|_BV(PD4);
 	TCCR1A |= _BV(WGM11)|_BV(COM1A1)|_BV(COM1A0);
 	TCCR1B |= _BV(WGM13)|_BV(WGM12)|_BV(CS10);
+
+	TIMSK |= _BV(OCIE1A)|_BV(TOIE1);
+
+	TIFR |= _BV(TOV1)|_BV(OCF1A);
+
 	ICR1 = 19999;
 
 	OCR1A = ICR1 - 2000;
+
+	sei();
 
 	while(1)
 	{

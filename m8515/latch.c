478839d6af600e49a724f9be02bfefd86fc109fb
Art Navsegda <artnavsegda@gmail.com>
2018-03-22 01:14:54 +0300

    latch

--- m8515/latch.c
+++ m8515/latch.c
@@ -6,23 +6,26 @@ int main(void)
 {
 	DDRA = 0xFF; // PORTA output
 	DDRB |= _BV(PB3); // latch strobe input
+	DDRC |= _BV(PC7);
 
 	_delay_ms(1000);
 
 	while(1)
 	{
 		PORTA |= _BV(PA2);
+		_delay_ms(100);
 		PORTB &= ~_BV(PB3); // drop front low
-		_delay_us(1);
+		_delay_ms(100);
 		PORTB |= _BV(PB3); // raise front high
 		
-		_delay_ms(100);
+		_delay_ms(1000);
 
 		PORTA &= ~_BV(PA2);
+		_delay_ms(100);
 		PORTB &= ~_BV(PB3); // drop front low
-		_delay_us(1);
+		_delay_ms(100);
 		PORTB |= _BV(PB3); // raise front high
 
-		_delay_ms(100);
+		_delay_ms(1000);
 	}
 }
